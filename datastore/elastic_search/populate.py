c94c6f74afd3845eb077b80e18ba1704779a5c0b
chiragjn <jain.chirag925@gmail.com>
2017-03-20 16:39:28 +0530

    Add documentation to elastic_search populate functions

--- datastore/elastic_search/populate.py
+++ datastore/elastic_search/populate.py
@@ -9,49 +9,45 @@ log_prefix = 'datastore.elastic_search.populate'
 
 def create_all_dictionary_data(connection, index_name, doc_type, entity_data_directory_path, logger, **kwargs):
     """
-    This setter function create entire model by going through all the files
-    :return:
+    Indexes all entity data from csv files stored at entity_data_directory_path, one file at a time
+    Args:
+        connection: Elasticsearch client object
+        index_name: The name of the index
+        doc_type: The type of the documents being indexed
+        logger: logging object to log at debug and exception level
+        entity_data_directory_path: Path of the directory containing the entity data csv files
+        kwargs:
+            Refer http://elasticsearch-py.readthedocs.io/en/master/helpers.html#elasticsearch.helpers.bulk
+
     """
     logger.debug('%s: +++ Started: create_all_dictionary_data() +++' % log_prefix)
     if entity_data_directory_path:
         logger.debug('%s: \t== Fetching from variants/ ==' % log_prefix)
-        create_dictionary_data_from_variants(connection=connection, index_name=index_name, doc_type=doc_type,
-                                             variants_directory_path=entity_data_directory_path, update=False,
-                                             logger=logger, **kwargs)
+        create_dictionary_data_from_directory(connection=connection, index_name=index_name, doc_type=doc_type,
+                                              entity_data_directory_path=entity_data_directory_path, update=False,
+                                              logger=logger, **kwargs)
         logger.debug('%s: +++ Finished: create_all_dictionary_data() +++' % log_prefix)
 
 
-def create_dictionary_data_from_variants(connection, index_name, doc_type, variants_directory_path, update, logger,
-                                         **kwargs):
+def get_variants_dictionary_value_from_key(csv_file_path, dictionary_key, logger, **kwargs):
     """
-    Creates the dictionary data from variants/
-    :return:
-    """
-    dict_type = DICTIONARY_DATA_VARIANTS
-    csv_files = get_files_from_directory(variants_directory_path)
-    for dictionary_key_file in csv_files:
-        dictionary_key = os.path.splitext(dictionary_key_file)[0]
-        dictionary_value = get_variants_dictionary_value_from_key(variants_directory_path=variants_directory_path,
-                                                                  dictionary_key=dictionary_key, logger=logger,
-                                                                  **kwargs)
-        if dictionary_value:
-            add_data_elastic_search(connection=connection, index_name=index_name, doc_type=doc_type,
-                                    dictionary_key=dictionary_key, dict_type=dict_type,
-                                    dictionary_value=remove_duplicate_data(dictionary_value),
-                                    update=update, logger=logger, **kwargs)
+    Reads the csv file at csv_file_path and create a dictionary mapping entity value to a list of their variants.
+    the entity values are first column of the csv file and their corresponding variants are stored in the second column
+    delimited by '|'
 
+    Args:
+        csv_file_path: absolute file path of the csv file populate entity data from
+        dictionary_key: name of the entity to be put the values under
+        logger: logging object to log at debug and exception level
+        kwargs:
+            Refer http://elasticsearch-py.readthedocs.io/en/master/helpers.html#elasticsearch.helpers.bulk
 
-def get_variants_dictionary_value_from_key(variants_directory_path, dictionary_key, logger, **kwargs):
-    """
-    This function will get the dictionary values given dictionary key from variants
-    :param logger:
-    :param variants_directory_path:
-    :param dictionary_key: cuisine,establishment_type etc
-    :return:
+    Returns:
+        Dictionary mapping entity value to a list of their variants.
     """
     dictionary_value = defaultdict(list)
     try:
-        csv_reader = read_csv(os.path.join(variants_directory_path, dictionary_key + '.csv'))
+        csv_reader = read_csv(csv_file_path)
         next(csv_reader)
         for data_row in csv_reader:
             try:
@@ -71,31 +67,47 @@ def get_variants_dictionary_value_from_key(variants_directory_path, dictionary_k
     return dictionary_value
 
 
-def add_data_elastic_search(connection, index_name, doc_type, dictionary_key, dict_type, dictionary_value, logger,
-                            update=False,
-                            **kwargs):
+def add_data_elastic_search(connection, index_name, doc_type, dictionary_key, dictionary_value, logger,
+                            update=False, **kwargs):
     """
-    Generates a query for bulk operation and adds it to elastic search
-    :param logger:
-    :param index_name:
-    :param connection:
-    :param dictionary_key:
-    :param dict_type:
-    :param dictionary_value:
-    :param update:
-    :return:
+    Adds all entity values and their variants to the index. Entity value and its list of variants are keys and values of
+    dictionary_value parameter generated from the csv file of this entity
+
+    Args:
+        connection: Elasticsearch client object
+        index_name: The name of the index
+        doc_type:  The type of the documents being indexed
+        dictionary_key: file name of the csv file without the extension, also used as the entity name to index values
+                        of this type. Example - 'city'
+        dictionary_value: dictionary, mapping entity value to a list of its variants.
+                            Example - 'New Delhi': ['Delhi', 'new deli', 'New Delhi']
+        logger: logging object to log at debug and exception level
+        update: boolean, True if this is a update type operation, False if create/index type operation
+        kwargs:
+            Refer http://elasticsearch-py.readthedocs.io/en/master/helpers.html#elasticsearch.helpers.bulk
+
+    Example of underlying index query
+        {'_index': 'index_name',
+         '_type': 'dictionary_data',
+         'dict_type': 'variants',
+         'entity_data': 'city',
+         'value': 'Baripada Town'',
+         'variants': ['Baripada', 'Baripada Town', '']
+         '_op_type': 'index'
+         }
+
     """
     str_query = []
     for value in dictionary_value:
         query_dict = {'_index': index_name,
                       'entity_data': dictionary_key,
-                      'dict_type': dict_type,
+                      'dict_type': DICTIONARY_DATA_VARIANTS,
                       'value': value,
                       'variants': dictionary_value[value],
                       '_type': doc_type
                       }
         if not update:
-            query_dict['_op_type'] = 'create'
+            query_dict['_op_type'] = 'index'
         str_query.append(query_dict)
         if len(str_query) > ELASTICSEARCH_BULK_HELPER_MESSAGE_SIZE:
             result = helpers.bulk(connection, str_query, stats_only=True, **kwargs)
@@ -104,3 +116,50 @@ def add_data_elastic_search(connection, index_name, doc_type, dictionary_key, di
     if str_query:
         result = helpers.bulk(connection, str_query, stats_only=True, **kwargs)
         logger.debug('%s: \t++ %s status %s ++' % (log_prefix, dictionary_key, result))
+
+
+def create_dictionary_data_from_file(connection, index_name, doc_type, csv_file_path, update, logger, **kwargs):
+    """
+    Indexes all entity data from the csv file at path csv_file_path
+    Args:
+        connection: Elasticsearch client object
+        index_name: The name of the index
+        doc_type:  The type of the documents being indexed
+        csv_file_path: absolute file path of the csv file to populate entity data from
+        update: boolean, True if this is a update type operation, False if create/index type operation
+        logger: logging object to log at debug and exception level
+        kwargs:
+            Refer http://elasticsearch-py.readthedocs.io/en/master/helpers.html#elasticsearch.helpers.bulk
+    """
+    dictionary_key = os.path.splitext(csv_file_path)[0]
+    dictionary_value = get_variants_dictionary_value_from_key(csv_file_path=csv_file_path,
+                                                              dictionary_key=dictionary_key, logger=logger,
+                                                              **kwargs)
+    if dictionary_value:
+        add_data_elastic_search(connection=connection, index_name=index_name, doc_type=doc_type,
+                                dictionary_key=dictionary_key,
+                                dictionary_value=remove_duplicate_data(dictionary_value),
+                                update=update, logger=logger, **kwargs)
+    if os.path.exists(csv_file_path) and os.path.splitext(csv_file_path)[1] == '.csv':
+        os.path.basename(csv_file_path)
+
+
+def create_dictionary_data_from_directory(connection, index_name, doc_type, entity_data_directory_path, update, logger,
+                                          **kwargs):
+    """
+    Wrapper function to call create_dictionary_data_from_file() for each csv file stored in entity_data_directory_path
+    Args:
+        connection: Elasticsearch client object
+        index_name: The name of the index
+        doc_type:  The type of the documents being indexed
+        entity_data_directory_path: Path of the directory containing the entity data csv files
+        update: boolean, True if this is a update type operation, False if create/index type operation
+        logger: logging object to log at debug and exception level
+        kwargs:
+            Refer http://elasticsearch-py.readthedocs.io/en/master/helpers.html#elasticsearch.helpers.bulk
+    """
+    csv_files = get_files_from_directory(entity_data_directory_path)
+    for csv_file in csv_files:
+        csv_file_path = os.path.join(entity_data_directory_path, csv_file)
+        create_dictionary_data_from_file(connection=connection, index_name=index_name, doc_type=doc_type,
+                                         csv_file_path=csv_file_path, update=update, logger=logger, **kwargs)
