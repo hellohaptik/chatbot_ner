592eebd4c7f9dec76f614a629ca40d5c6335b1f2
“KHUSHAL <2014A7PS094G@localhost>
2017-06-13 21:44:43 +0530

    changed input params from file to list. changed for func get_crf_output() --

--- models/crf/read_model.py
+++ models/crf/read_model.py
@@ -48,3 +48,64 @@ class GenerateCRFTrain(object):
         self.initialize_files('city')
         self.add_data_to_tagger(bot_message, user_message)
         self.run()
+
+
+    def generate_city_output(self, crf_output_list):
+        file_name = OUTPUT+filename+OUTPUT_FORMAT
+        departure_city = ''
+        arrival_city = ''
+        via_city = ''
+        list_json = []
+        read_line = 0
+        while read_line<(len(crf_output_list)-1):
+            if crf_output_list[read_line][3]=='O':
+                # print reader[read_line]
+                pass
+            else:
+                temp_list = self.check_label(reader_list=crf_output_list, reader_pointer=read_line, begin_label=CITY_FROM_B, inner_label=CITY_FROM_I, from_bool=1, to_bool=0, via_bool=0)
+
+                if temp_list is not None:
+                    list_json.append(temp_list)
+
+                else:
+                    temp_list = self.check_label(reader_list=crf_output_list, reader_pointer=read_line, begin_label=CITY_TO_B, inner_label=CITY_TO_I, from_bool=0, to_bool=1, via_bool=0)
+                    if temp_list is not None:
+                        list_json.append(temp_list)
+
+                    else:
+                        temp_list = self.check_label(reader_list=crf_output_list, reader_pointer=read_line, begin_label=CITY_VIA_B, inner_label=CITY_VIA_I, from_bool=0, to_bool=0, via_bool=1)
+                        if temp_list is not None:
+                            list_json.append(temp_list)
+            read_line+=1
+        return list_json
+
+
+    def make_json(self, city_value, from_bool, to_bool, via_bool):
+        pythonDict = {ENTITY: city_value, FROM: from_bool, TO: to_bool, VIA: via_bool}
+        return (json.dumps(pythonDict))
+
+    def check_label(self, reader_list, reader_pointer, begin_label, inner_label, from_bool, to_bool, via_bool):
+        list_json = []
+        xyz_city = ''
+        # print reader_list[reader_pointer]
+        # print begin_label
+        # print type(begin_label)
+        if reader_list[reader_pointer][3]==begin_label:
+            # print reader_list[reader_pointer]
+            xyz_city = reader_list[reader_pointer][0]
+            if reader_pointer==(len(reader_list)-2):
+                list_json.append(self.make_json(city_value=xyz_city, from_bool=from_bool, to_bool=to_bool, via_bool=via_bool))
+                return list_json
+
+            else:
+                check_pointer = reader_pointer+1
+                while check_pointer < (len(reader_list)-1):
+                    if reader_list[check_pointer][3]!=inner_label:
+                        list_json.append(self.make_json(city_value=xyz_city, from_bool=from_bool, to_bool=to_bool, via_bool=via_bool))
+                        return list_json
+                    else:
+                        xyz_city = xyz_city +' '+ reader_list[check_pointer][0]
+                        check_pointer+=1
+                        if check_pointer==(len(reader_list)-2):
+                            list_json.append(self.make_json(city_value=xyz_city, from_bool=from_bool, to_bool=to_bool, via_bool=via_bool))
+                            return list_json
