9f6a05c3e0029fa298be35695355b74a3d365495
ranvijayj <ranvijayj@gmail.com>
2017-06-15 14:55:15 +0530

    Create install_CRF.sh

---
+++ docker/install_CRF.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/model_lib
+cd /tmp/
+wget ftp://ftp.netbsd.org/pub/pkgsrc/distfiles/CRF++-0.58.tar.gz
+tar -xzf CRF++-0.58.tar.gz -C ~/model_lib/
+cd ~/model_lib/CRF++-0.58/
+./configure
+make
+sudo make install
+
+echo "export LD_LIBRARY_PATH=/usr/local/lib" >> /home/ubuntu/.bashrc
+
+cd python
+python setup.py build
+python setup.py install
