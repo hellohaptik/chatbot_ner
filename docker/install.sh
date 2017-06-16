b051540ea3712d543ab2ed7b3ae3f16ab1bbdc06
root <root@ip-172-30-0-234.us-west-2.compute.internal>
2017-06-16 13:09:06 +0000

    latest files June 16, docker file and install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-pip install pip --upgrade
-pip install -U virtualenvwrapper
-/bin/bash -c "source /usr/local/bin/virtualenvwrapper.sh;  mkvirtualenv chatbotnervenv ;  workon chatbotnervenv"
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
-echo "workon chatbotnervenv" >> ~/.bashrc
 source ~/.bashrc
-cd ~/ &&  git clone https://github.com/hellohaptik/chatbot_ner.git --branch docker
+cd ~/ &&  git clone https://github.com/hellohaptik/chatbot_ner.git --branch master
 cd ~/chatbot_ner && pip install -r requirements.txt
 cp /root/chatbot_ner/config.example /root/chatbot_ner/config
 ~/chatbot_ner_elasticsearch/elasticsearch-2.4.4/bin/elasticsearch -Des.insecure.allow.root=true -d && sleep 10 && python ~/chatbot_ner/initial_setup.py
@@ -13,6 +8,7 @@ touch /usr/bin/killgun; echo "ps -aef | grep gunicorn | grep -v grep | cut -d ' 
 
 cd /tmp
 wget "https://s3-us-west-2.amazonaws.com/chatbotner/chatbot_ner_nginx/default"
-bash -c "/tmp/default > /etc/nginx/sites-available/default"
-rm -rf /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+sudo bash -c "cat /tmp/default > /etc/nginx/sites-available/default"
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+sudo sed -i 's/www-data/root/g' /etc/nginx/nginx.conf
