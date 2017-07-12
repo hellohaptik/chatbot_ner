0a18867388bbc9a6a41c4cc3ee0113cd8e308e59
“KHUSHAL <2014A7PS094G@localhost>
2017-07-12 12:20:37 +0530

    city.py modified as per new json response

--- models/crf/output_generation/city.py
+++ models/crf/output_generation/city.py
@@ -10,12 +10,13 @@ def generate_city_output(crf_data):
         user_message = 'flights for delhi to goa'
 
         Then run_crf will give the following output:
-            crf_data = [['none','O'], ['flights', 'O'], ['from', 'O'], ['delhi', 'from-B'], ['to', 'O'],
-            ['goa', 'to-B']]
+            crf_data = [['none','O'], ['flights', 'O'], ['from', 'O'], ['delhi', 'FROM-B'], ['to', 'O'],
+            ['goa', 'TO-B']]
 
         Calling generate_city_output with crf_output_list will extract required text and return a json in the
         following form:
-            [[{'city': 'delhi', 'via': 0, 'from': 1, 'to': 0}], [{'city': 'goa', 'via': 0, 'from': 0, 'to': 1}]]
+            [[{'city': 'delhi', 'via': False, 'from': True, 'to': False, 'normal': False}], 
+                                        [{'city': 'goa', 'via': False, 'from': False, 'to': True, 'normal': False}]]
 
     This functions iterates over the list and calls the check_label which will return a json list of tagged words.
 
@@ -26,52 +27,63 @@ def generate_city_output(crf_data):
         if crf_data[read_line][1] == 'O':
             pass
         else:
-            temp_list = check_label(reader_list=crf_data, reader_pointer=read_line, begin_label=constants.CITY_FROM_B,
-                                    inner_label=constants.CITY_FROM_I, from_bool=1, to_bool=0, via_bool=0)
+            temp_list = check_label(reader_list=crf_data, reader_pointer=read_line,
+                                    begin_label=constants.CITY_FROM_B, inner_label=constants.CITY_FROM_I,
+                                    from_bool=True, to_bool=False, via_bool=False, normal_bool=False)
             if len(temp_list) is not 0:
                 list_json.append(temp_list)
 
             else:
                 temp_list = check_label(reader_list=crf_data, reader_pointer=read_line,
                                         begin_label=constants.CITY_TO_B, inner_label=constants.CITY_TO_I,
-                                        from_bool=0, to_bool=1, via_bool=0)
+                                        from_bool=False, to_bool=True, via_bool=False, normal_bool=False)
                 if len(temp_list) is not 0:
                     list_json.append(temp_list)
 
                 else:
                     temp_list = check_label(reader_list=crf_data, reader_pointer=read_line,
                                             begin_label=constants.CITY_VIA_B, inner_label=constants.CITY_VIA_I,
-                                            from_bool=0, to_bool=0, via_bool=1)
+                                            from_bool=False, to_bool=False, via_bool=True, normal_bool=False)
                     if len(temp_list) is not 0:
                         list_json.append(temp_list)
+
+                    else:
+                        temp_list = check_label(reader_list=crf_data, reader_pointer=read_line,
+                                                begin_label=constants.CITY_NORMAL_B, inner_label=constants.CITY_NORMAL_I,
+                                                from_bool=False, to_bool=False, via_bool=False, normal_bool=True)
+
+                        if len(temp_list) is not 0:
+                            list_json.append(temp_list)
         read_line += 1
     return list_json
 
 
-def make_json(city_value, from_bool, to_bool, via_bool):
+def make_json(city_value, from_bool, to_bool, via_bool, normal_bool):
     """
     This function return json of the value found in function check_label
 
     for Example:
         Args:
             city_value = 'delhi'
-            from_bool = 1
-            to_bool = 0
-            via_bool = 0
+            from_bool = True
+            to_bool = False
+            via_bool = False
+            normal_bool = False
 
         then calling this function with ab ove parameters will give:
-            {'city': 'delhi', 'via': 0, 'from': 1, 'to': 0}
+            {'city': 'delhi', 'via': False, 'from': True, 'to': False, 'normal': False}
     """
     python_dict = {
         constants.CITY_VALUE: city_value,
         constants.FROM: from_bool,
         constants.TO: to_bool,
         constants.VIA: via_bool
+        constants.NORMAL: normal_bool
     }
     return python_dict
 
 
-def check_label(reader_list, reader_pointer, begin_label, inner_label, from_bool, to_bool, via_bool):
+def check_label(reader_list, reader_pointer, begin_label, inner_label, from_bool, to_bool, via_bool, normal_bool):
     """
     this function checks if a particular word has been given a particular label
 
@@ -81,30 +93,31 @@ def check_label(reader_list, reader_pointer, begin_label, inner_label, from_bool
             reader_pointer = 3
             begin_label = CITY_FROM_B
             inner_label = CITY_FROM_I
-            from_bool = 1
-            to_bool = 0
-            via_bool = 0
+            from_bool = True
+            to_bool = False
+            via_bool = False
+            normal_bool = False
 
         When check_label is called with above parameters, it checks if the word has been given CITY_FROM_B label
         and its following words
         have CITY_FROM_I label. I so it returns the proper json.
         For above inputs it will return
-        [{'city': 'delhi', 'via': 0, 'from': 1, 'to': 0}]
+        [{'city': 'delhi', 'via': False, 'from': True, 'to': False, 'normal': False}]
     """
     list_json = []
     if reader_list[reader_pointer][1] == begin_label:
         entity_value = reader_list[reader_pointer][0]
         if reader_pointer == (len(reader_list) - 1):
-            return make_json(city_value=entity_value, from_bool=from_bool, to_bool=to_bool, via_bool=via_bool)
+            return make_json(city_value=entity_value, from_bool=from_bool, to_bool=to_bool, via_bool=via_bool, normal_bool=normal_bool)
         else:
             check_pointer = reader_pointer + 1
             while check_pointer < (len(reader_list)):
                 if reader_list[check_pointer][1] != inner_label:
-                    return make_json(city_value=entity_value, from_bool=from_bool, to_bool=to_bool, via_bool=via_bool)
+                    return make_json(city_value=entity_value, from_bool=from_bool, to_bool=to_bool, via_bool=via_bool, normal_bool=normal_bool)
                 else:
                     entity_value = entity_value + ' ' + reader_list[check_pointer][0]
                     check_pointer += 1
                     if check_pointer == (len(reader_list) - 1):
                         return make_json(city_value=entity_value, from_bool=from_bool, to_bool=to_bool,
-                                         via_bool=via_bool)
+                                         via_bool=via_bool, normal_bool=normal_bool)
     return list_json
