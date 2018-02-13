f78570a8b51230320d8580e38667a93a329d3199
jayaraopratik <pratik.jayarao@haptik.co>
2018-02-13 16:18:51 +0530

    Add parameter meta data in parameters_dict

--- ner_v1/api.py
+++ ner_v1/api.py
@@ -9,7 +9,7 @@ from ner_v1.chatbot.entity_detection import get_text, get_location, get_phone_nu
     get_number, get_shopping_size, get_time, get_date, get_budget, get_person_name, get_regex
 from ner_v1.chatbot.tag_message import run_ner
 from ner_v1.constant import PARAMETER_MESSAGE, PARAMETER_ENTITY_NAME, PARAMETER_STRUCTURED_VALUE, \
-    PARAMETER_FALLBACK_VALUE, PARAMETER_BOT_MESSAGE, PARAMETER_TIMEZONE
+    PARAMETER_FALLBACK_VALUE, PARAMETER_BOT_MESSAGE, PARAMETER_TIMEZONE, PARAMETER_META_DATA
 
 
 def get_parameters_dictionary(request):
@@ -27,7 +27,8 @@ def get_parameters_dictionary(request):
                        PARAMETER_STRUCTURED_VALUE: request.GET.get('structured_value'),
                        PARAMETER_FALLBACK_VALUE: request.GET.get('fallback_value'),
                        PARAMETER_BOT_MESSAGE: request.GET.get('bot_message'),
-                       PARAMETER_TIMEZONE: request.GET.get('timezone')}
+                       PARAMETER_TIMEZONE: request.GET.get('timezone'),
+                       PARAMETER_META_DATA: request.GET.get('meta_data')}
 
     return parameters_dict
 
@@ -109,7 +110,8 @@ def regex(request):
         entity_output = get_regex(parameters_dict[PARAMETER_MESSAGE], parameters_dict[PARAMETER_ENTITY_NAME],
                                          parameters_dict[PARAMETER_STRUCTURED_VALUE],
                                          parameters_dict[PARAMETER_FALLBACK_VALUE],
-                                         parameters_dict[PARAMETER_BOT_MESSAGE])
+                                         parameters_dict[PARAMETER_BOT_MESSAGE],
+                                         parameters_dict[PARAMETER_META_DATA])
         ner_logger.debug('Finished %s : %s ' % (parameters_dict[PARAMETER_ENTITY_NAME], entity_output))
     except TypeError, e:
         ner_logger.debug('Exception for phone_number: %s ' % e)
