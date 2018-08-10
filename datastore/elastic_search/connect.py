9729591fe438ff8861ab6bfe0fa71c24925f207a
jayaraopratik <pratik.jayarao@haptik.co>
2018-08-10 14:16:15 +0530

    Add alias handling in connect

--- datastore/elastic_search/connect.py
+++ datastore/elastic_search/connect.py
@@ -1,5 +1,7 @@
 from elasticsearch import Elasticsearch
-
+import requests
+from chatbot_ner.config import CHATBOT_NER_DATASTORE
+import json
 log_prefix = 'datastore.elastic_search.connect'
 
 
@@ -35,3 +37,44 @@ def connect(connection_url=None, host=None, port=None, user=None, password=None,
         connection = None
 
     return connection
+
+
+class FetchIndexForAliasException(Exception):
+    """
+    This exception is raised if fetch for indices for an alias fails
+    """
+    pass
+
+
+def _get_current_live_index(alias_name):
+    es_scheme = ''
+    es_url = (es_scheme + "://" +
+                          CHATBOT_NER_DATASTORE.get('host') + ":" +
+                          CHATBOT_NER_DATASTORE.get('port'))
+    current_live_index = fetch_index_alias_points_to(es_url, alias_name)
+    return current_live_index
+
+
+def fetch_index_alias_points_to(es_url, alias_name):
+    """
+    This function fetches the current live index, ie the index the alias points to
+
+    Args
+        es_url (str): The elasticsearch URL
+        alias_name (str):  The name of alias for which the list of indices has to be fetched
+
+    Returns
+        current_live_index (str): The current live index
+    """
+    response = requests.get(es_url + '/*/_alias/' + alias_name)
+    if response.status_code == 200:
+        json_obj = json.loads(response.content)
+        indices = json_obj.keys()
+        if '' in indices:
+            return ''
+        elif '' in indices:
+            return ''
+        else:
+            raise FetchIndexForAliasException(alias_name)
+    raise FetchIndexForAliasException('fetch index for ' + alias_name + ' failed')
+
