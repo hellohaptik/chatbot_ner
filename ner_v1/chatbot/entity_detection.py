c5867743fdc1acfcb3ba7b1710e235e2a45807e1
Sailesh Dev <saileshdev@gmail.com>
2018-08-24 12:54:54 +0530

    Trigger detect method of EmailDectector

--- ner_v1/chatbot/entity_detection.py
+++ ner_v1/chatbot/entity_detection.py
@@ -319,20 +319,8 @@ def get_email(message, entity_name, structured_value, fallback_value, bot_messag
 
     """
     email_detection = EmailDetector(entity_name=entity_name)
-    if structured_value:
-        entity_list, original_text_list = email_detection.detect_entity(text=structured_value)
-        if entity_list:
-            return output_entity_dict_list(entity_list, original_text_list, FROM_STRUCTURE_VALUE_VERIFIED)
-        else:
-            return output_entity_dict_list([structured_value], [structured_value], FROM_STRUCTURE_VALUE_NOT_VERIFIED)
-    else:
-        entity_list, original_text_list = email_detection.detect_entity(text=message)
-        if entity_list:
-            return output_entity_dict_list(entity_list, original_text_list, FROM_MESSAGE)
-        elif fallback_value:
-            return output_entity_dict_list([fallback_value], [fallback_value], FROM_FALLBACK_VALUE)
-
-    return None
+    return email_detection.detect(message=message, structured_value=structured_value, fallback_value=fallback_value,
+                                  bot_message=bot_message)
 
 
 def get_city(message, entity_name, structured_value, fallback_value, bot_message):
