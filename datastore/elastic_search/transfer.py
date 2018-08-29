3c850ad9059169eccffa0a3d6937e1d44299ae5f
jayaraopratik <pratik.jayarao@haptik.co>
2018-08-29 11:58:21 +0530

    Add wait for completion and refresh as true

--- datastore/elastic_search/transfer.py
+++ datastore/elastic_search/transfer.py
@@ -378,7 +378,7 @@ class ESTransfer(object):
             }
         }
         reindex_response = requests.post('{es_url}/_reindex'.format(**{'es_url': es_url}), json=final_request_dict,
-                                         params={"refresh": "wait_for"})
+                                         params={"refresh": "true", "wait_for_completion": "true"})
         if reindex_response.status_code != 200:
             message = "transfer from " + index_to_backup + "to " + backup_index + " failed"
             raise InternalBackupException(message)
