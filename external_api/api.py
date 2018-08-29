cfc6c295d1ca53b3faa0fc1b783b408ae1949491
jayaraopratik <pratik.jayarao@haptik.co>
2018-08-29 17:07:42 +0530

    Add decorater

--- external_api/api.py
+++ external_api/api.py
@@ -10,6 +10,7 @@ from datastore.exceptions import IndexNotFoundException, InvalidESURLException, 
     FetchIndexForAliasException, DeleteIndexFromAliasException
 from chatbot_ner.config import ner_logger
 from external_api.constants import ENTITY_DATA, ENTITY_NAME, LANGUAGE_SCRIPT, ENTITY_LIST, EXTERNAL_API_DATA
+from django.views.decorators.csrf import csrf_exempt
 
 
 def get_entity_word_variants(request):
@@ -52,7 +53,7 @@ def get_entity_word_variants(request):
 
     return HttpResponse(json.dumps(response), content_type='application/json', status=200)
 
-
+@csrf_exempt
 def update_dictionary(request):
     """
     This function is used to update the dictionary entities.
