1f3e93fef453dc0cff424829bd2fad321c110b9a
jayaraopratik <pratik.jayarao@haptik.co>
2018-08-31 19:37:43 +0530

    Add loggers to detect entities

--- models/crf_v2/get_crf_tagger.py
+++ models/crf_v2/get_crf_tagger.py
@@ -1,7 +1,7 @@
 from lib.singleton import Singleton
 from lib.redis_utils import get_cache_ml
 from .constants import ENTITY_REDIS_MODELS_PATH
-from chatbot_ner.config import AWS_MODEL_REGION, AWS_MODEL_BUCKET
+from chatbot_ner.config import AWS_MODEL_REGION, AWS_MODEL_BUCKET, ner_logger
 from lib.aws_utils import read_model_dict_from_s3
 import pycrfsuite
 
@@ -25,21 +25,24 @@ class CrfModel(object):
         if model_path:
             file_handler = open(model_path, 'r')
             self.entity_model_dict = file_handler.read()
+            ner_logger.debug('Model dir %s path from local %s' % model_path)
             return self.initialize_tagger()
 
         s3_model_path = get_cache_ml(ENTITY_REDIS_MODELS_PATH + self.entity_name)
-
+        ner_logger.debug('Model dir %s path from cache %s' % s3_model_path)
         if s3_model_path == self.loaded_model_path:
             if not self.entity_model_dict:
                 self.entity_model_dict = read_model_dict_from_s3(bucket_name=AWS_MODEL_BUCKET,
                                                                  bucket_region=AWS_MODEL_REGION,
                                                                  model_path_location=s3_model_path)
+                ner_logger.debug('New Model dir %s path from cache %s' % s3_model_path)
             else:
                 return self.tagger
         else:
             self.entity_model_dict = read_model_dict_from_s3(bucket_name=AWS_MODEL_BUCKET,
                                                              bucket_region=AWS_MODEL_REGION,
                                                              model_path_location=s3_model_path)
+            ner_logger.debug('New Model dir %s path from cache %s' % s3_model_path)
             self.loaded_model_path = s3_model_path
         return self.initialize_tagger()
 
