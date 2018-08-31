17026e1d27b5801e5203c2c19e3b1945d1a7aad1
jayaraopratik <pratik.jayarao@haptik.co>
2018-08-31 11:19:54 +0530

    Add functions for cache in temp file

---
+++ models/crf_v2/redis_storage_models.py
@@ -0,0 +1,19 @@
+from django.core.cache import caches
+cached_ml = caches['redis_ml']
+
+
+def set_cache_ml_dest(variable_name, variable_value, expiry_time=172800):
+    """
+    Stores the cache data in a cache
+    """
+    cached_ml.set(variable_name, variable_value, expiry_time)
+
+
+def get_cache_ml(variable_name):
+    """
+    Gets the data from the cache
+    if data is not present then it will return None
+    :return:
+    """
+    response = cached_ml.get(variable_name)
+    return response
