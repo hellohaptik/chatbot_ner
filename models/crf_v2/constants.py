6ac20fea6295c04b09ba8c2a1bddd874407a4d3f
jayaraopratik <pratik.jayarao@haptik.co>
2018-09-02 09:56:05 +0530

    Add embeddings path

--- models/crf_v2/constants.py
+++ models/crf_v2/constants.py
@@ -1,6 +1,6 @@
 ENTITY_LIST = 'entity_list'
 TEXT_LIST = 'text_list'
-EMBEDDINGS_PATH_VOCAB = ''
-EMBEDDINGS_PATH_VECTORS = ''
+EMBEDDINGS_PATH_VOCAB = '/root/models/models_live/nearby_glove_pos'
+EMBEDDINGS_PATH_VECTORS = '/root/models/models_live/nearby_glove_vectors_pos'
 
 ENTITY_REDIS_MODELS_PATH = 'entity_model_path|'
