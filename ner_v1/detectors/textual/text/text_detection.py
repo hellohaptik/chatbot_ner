d581fd33f620f79036267eee5bcdb57c395fa18a
jayaraopratik <pratik.jayarao@haptik.co>
2018-09-07 12:40:50 +0530

    Add add_verification_source to add the verification source for the detected entity values

--- ner_v1/detectors/textual/text/text_detection.py
+++ ner_v1/detectors/textual/text/text_detection.py
@@ -9,6 +9,8 @@ from lib.nlp.levenshtein_distance import edit_distance
 # from lib.nlp.regexreplace import RegexReplace
 from ner_v1.detectors.base_detector import BaseDetector
 from ner_v1.language_utilities.constant import ENGLISH_LANG, HINDI_LANG
+from ner_v1.detectors.constant import ES_VERIFIED
+from ner_v1.constant import ENTITY_VALUE_DICT_KEY
 
 
 class TextDetector(BaseDetector):
@@ -295,7 +297,9 @@ class TextDetector(BaseDetector):
 
         values, original_texts = self._text_detection_with_variants()
 
-        self.text_entity_values, self.original_texts = values, original_texts
+        text_entity_verified_values = TextDetector.add_verification_source(values=values,
+                                                                           verification_source_dict={ES_VERIFIED: True})
+        self.text_entity_values, self.original_texts = text_entity_verified_values, original_texts
 
         return self.text_entity_values, self.original_texts
 
@@ -402,3 +406,30 @@ class TextDetector(BaseDetector):
                 original_text_tokens = []
                 variant_token_i = 0
         return None
+
+    @staticmethod
+    def add_verification_source(values, verification_source_dict):
+        """
+        Add the verification source for the detected entities
+        Args:
+            values (list): List of detected text type entities
+            verification_source_dict (dict): Dict consisting of the verification source and value.
+        Returns:
+            text_entity_verified_values (list): List of dicts consisting of the key and values for the keys
+            value and verification source
+        Example:
+            values = [u'Chennai', u'New Delhi', u'chennai']
+            verification_source_dict = {"es_verified": True}
+
+            >> add_verification_source(values, verification_source_dict)
+                [{'es_verified': True, 'value': u'Chennai'},
+                 {'es_verified': True, 'value': u'New Delhi'},
+                 {'es_verified': True, 'value': u'chennai'}]
+        """
+        text_entity_verified_values = []
+        for text_entity_value in values:
+            text_entity_dict = {ENTITY_VALUE_DICT_KEY: text_entity_value}
+            for verification_source_key in list(verification_source_dict.keys()):
+                text_entity_dict[verification_source_key] = verification_source_dict[verification_source_key]
+            text_entity_verified_values.append(text_entity_dict)
+        return text_entity_verified_values
