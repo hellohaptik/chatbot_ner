fca4404157047b04a1acbd6690b4e3960e73a29c
amansrivastav17 <amans.rlx@gmail.com>
2018-10-25 14:40:08 +0530

    review changes for "Put inline comments for constants from lines 8 to 15"

--- ner_v2/detectors/temporal/constant.py
+++ ner_v2/detectors/temporal/constant.py
@@ -5,13 +5,28 @@ NUMERALS_CONSTANT_FILE = 'numbers_constant.csv'
 
 CONSTANT_FILE_KEY = 'key'
 
+# date type referring to date in month like "2 tarikh" (reference: hindi)
 MONTH_DATE_REF_TYPE = 'month_date_ref'
+
+# date type referring to word like "kal", "aaj" (reference: hindi)
 RELATIVE_DATE = 'relative_date'
+
+# date type referring to literal word for day like "din", "dino" (reference: hindi)
 DATE_LITERAL_TYPE = 'date_literal'
+
+# date type referring to literal word for month like "mahina", "mahino" (reference: hindi)
 MONTH_LITERAL_TYPE = 'month_literal'
+
+# date type referring to weekdays like "friday", "monday", "sunday"
 WEEKDAY_TYPE = 'weekday'
+
+# date type referring to month of year like "jan", "feb"
 MONTH_TYPE = 'month'
+
+# datetime_diff type referring to words like "baad", "pehle", "pichhla"
 ADD_DIFF_DATETIME_TYPE = 'add_diff_datetime'
+
+# datetime_diff type referring to words like "sava", "paune"
 REF_DATETIME_TYPE = 'ref_datetime'
 
 HOUR_TIME_TYPE = 'hour'
