08ef80cfffecd7f95d92d61ff8fe27d63541cf3d
viraj <anchan.viraj@gmail.com>
2018-11-01 12:05:23 +0530

    remove nginx
    remove nginx

--- docker/install.sh
+++ docker/install.sh
@@ -16,9 +16,9 @@ python setup.py install
 
 # Get sample Nginx file for routing and GUI
 
-cd /tmp
-wget "https://s3-us-west-2.amazonaws.com/chatbotner/chatbot_ner_nginx/default"
-bash -c "cat /tmp/default > /etc/nginx/sites-available/default"
-rm -rf /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-sed -i 's/www-data/root/g' /etc/nginx/nginx.conf
+# cd /tmp
+# wget "https://s3-us-west-2.amazonaws.com/chatbotner/chatbot_ner_nginx/default"
+# bash -c "cat /tmp/default > /etc/nginx/sites-available/default"
+# rm -rf /etc/nginx/sites-enabled/default
+# ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+# sed -i 's/www-data/root/g' /etc/nginx/nginx.conf
