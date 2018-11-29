d6401fad74268d6a332fbaf988d0e850b2ba5671
amansrivastav17 <amans.rlx@gmail.com>
2018-11-29 13:20:26 +0530

    added check for new date range in advance date detector

--- ner_v2/detectors/temporal/date/date_detection.py
+++ ner_v2/detectors/temporal/date/date_detection.py
@@ -200,9 +200,31 @@ class DateAdvancedDetector(BaseDetector):
                                    r'(?:of)?'
                                    r'\s?'
                                    r'([A-Za-z]+))\b', flags=re.UNICODE)
+
+        regex_pattern_2 = re.compile(r'\b(([12][0-9]|3[01]|0?[1-9])'
+                                     r'\s?'
+                                     r'(?:nd|st|rd|th)?'
+                                     r'\s?'
+                                     r'([A-Za-z]+)'
+                                     r'\s?'
+                                     r'(?:to|\-|till)'
+                                     r'\s?'
+                                     r'([12][0-9]|3[01]|0?[1-9])'
+                                     r'\s?'
+                                     r'(?:nd|st|rd|th)?)\b', flags=re.UNICODE)
+
         patterns = regex_pattern.findall(self.processed_text.lower())
+        patterns_2 = regex_pattern_2.findall(self.processed_text.lower())
         if patterns:
             for pattern in patterns:
+                date_dict_list.extend(
+                    self._date_dict_from_text(text=pattern[0])
+                )
+            date_dict_list[0][detector_constant.DATE_START_RANGE_PROPERTY] = True
+            date_dict_list[-1][detector_constant.DATE_END_RANGE_PROPERTY] = True
+
+        if patterns_2:
+            for pattern in patterns_2:
                 date_dict_list.extend(
                     self._date_dict_from_text(text=pattern[0])
                 )
