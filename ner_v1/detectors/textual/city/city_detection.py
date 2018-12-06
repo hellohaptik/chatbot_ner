75da40b08475410fdd6a8bb942cb35c34ce7c5ca
amansrivastav17 <amans.rlx@gmail.com>
2018-12-06 13:28:24 +0530

    add unicode flag in regex to detect arrival date pattern

--- ner_v1/detectors/textual/city/city_detection.py
+++ ner_v1/detectors/textual/city/city_detection.py
@@ -263,7 +263,8 @@ class CityDetector(object):
         patterns_1 = re.findall(r'\s((to|2|for|fr|arriving|arrive|reaching|reach|rch|destination city\:|arrival city\:)'
                                 r'\s+([A-Za-z]+))\.?\s',
                                 self.processed_text.lower())
-        patterns_2 = re.findall(r'(.+)\s+(jana|jaana|jau|ghum|ghoom|जाना|जाऊं|जाऊँ|घूम)', self.processed_text.lower())
+        patterns_2 = re.findall(r'(.+)\s+(jana|jaana|jau|ghum|ghoom|जाना|जाऊं|जाऊँ|घूम)', self.processed_text.lower(),
+                                re.UNICODE)
         for pattern in patterns_1:
             city_dict_list.extend(
                 self._city_dict_from_text(text=pattern[2], to_property=True)
