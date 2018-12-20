91b686f1647c6d2b5562099202533cedb35fb625
jayaraopratik <pratik.jayarao@haptik.co>
2018-12-20 14:24:50 +0530

    Add unit tests and remove number detection

--- ner_v2/detectors/pattern/phone_number/phone_number_detection.py
+++ ner_v2/detectors/pattern/phone_number/phone_number_detection.py
@@ -6,10 +6,7 @@ import re
 
 class PhoneDetector(BaseDetector):
     def __init__(self, entity_name, language=ENGLISH_LANG):
-
-        self.number_detector = NumberDetector(language=language, entity_name=entity_name)
         self._supported_languages = NumberDetector.get_supported_languages()
-
         super(PhoneDetector, self).__init__(language)
         self.language = language
         self.entity_name = entity_name
@@ -29,38 +26,31 @@ class PhoneDetector(BaseDetector):
         self.processed_text = self.text
         self.tagged_text = self.text
 
-        self.number_detector.set_min_max_digits(8, 14)
         phone_number_original_list = self.get_number_regex()
 
-        original_phone_list = [p[0] for p in phone_number_original_list]
-        clean_phone_list = [self.clean_phone_number(p) for p in original_phone_list]
-
-        self.phone, self.original_phone_text = self.get_number(original_phone_list=original_phone_list,
-                                                               clean_phone_list=clean_phone_list)
-
+        self.original_phone_text = [p[0] for p in phone_number_original_list]
+        clean_phone_list = [self.clean_phone_number(p) for p in self.original_phone_text]
+        self.phone = [self.get_number(phone) for phone in clean_phone_list]
         self.get_tagged_text()
 
         return self.phone, self.original_phone_text
 
-    def get_number(self, clean_phone_list, original_phone_list):
-        phone = []
-        original_phone_text = []
+    def get_number(self, phone):
+        phone_length = len(phone)
+        phone = str(int(phone))
 
-        for each, each_i in zip(clean_phone_list, original_phone_list):
-            detected_number = self.number_detector.detect_entity(each)[0]
-            if detected_number:
-                phone.append(detected_number[0]['value'])
-                original_phone_text.append(each_i)
+        if phone_length != len(phone):
+            phone = phone.zfill(phone_length)
 
-        return phone, original_phone_text
+        return phone
 
     def clean_phone_number(self, number):
-        clean_regex = re.compile('[\+()\sext]+')
+        clean_regex = re.compile('[\+()\-\sext]+')
         return clean_regex.sub(string=number, repl='')
 
     def get_number_regex(self):
         phone_number_regex = re.compile(
-            r'((?:\(?\+(\d{1,2})\)?[\s\-\.]*)?((?=[\-\d()\s\.]{9,16}'
+            r'((?:\(?\+(\d{1,2})\)?[\s\-\.]*)?((?=[\-\d()\s\.]{8,16}'
             r'(?:\s*e?xt?\.?\s*(?:\d{1,20}))?(?:[^\d]+|$))'
             r'(?:[\d(]{1,20}(?:[\-)\s\.]*\d{1,20}){0,20}){1,20})'
             r'(?:\s*e?xt?\.?\s*(\d{1,20}))?)', re.U)
