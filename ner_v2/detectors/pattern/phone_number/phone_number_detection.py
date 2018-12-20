b2ce2ae0b105ff531a7fc0efb80eab1b13051126
jayaraopratik <pratik.jayarao@haptik.co>
2018-12-20 12:00:34 +0530

    Call number detection to convert original text to english value

--- ner_v2/detectors/pattern/phone_number/phone_number_detection.py
+++ ner_v2/detectors/pattern/phone_number/phone_number_detection.py
@@ -5,42 +5,54 @@ import re
 
 
 class PhoneDetector(BaseDetector):
-
     def __init__(self, entity_name, language=ENGLISH_LANG):
+
+        self.number_detector = NumberDetector(language=language, entity_name=entity_name)
         self._supported_languages = NumberDetector.get_supported_languages()
+
         super(PhoneDetector, self).__init__(language)
         self.language = language
-        self.number_detector = NumberDetector(entity_name=entity_name, language=self.language)
         self.entity_name = entity_name
         self.text = ''
         self.tagged_text = ''
         self.processed_text = ''
-        self.number = []
-        self.original_number_text = []
+        self.phone = []
+        self.original_phone_text = []
         self.tag = '__' + self.entity_name + '__'
 
     @property
     def supported_languages(self):
         return self._supported_languages
 
-    def get_number(self):
-        self.number_detector.set_min_max_digits(min_digit=8, max_digit=14)
-        self.number_detector.detect_entity(text=self.text)
-        number_list, original_number_list = self.number_detector.detect_entity(text=self.text)
-        return number_list, original_number_list
-
     def detect_entity(self, text, **kwargs):
-        self.text = text
+        self.text = text.decode('utf-8')
         self.processed_text = self.text
         self.tagged_text = self.text
-        # number_list, original_list = self.get_number()
-        return self.get_phone_number()
 
-    def get_phone_number(self):
+        self.number_detector.set_min_max_digits(8, 14)
         phone_number_original_list = self.get_number_regex()
-        phone_number_original_list = [p[0] for p in phone_number_original_list]
-        phone_number_list = [self.clean_phone_number(p) for p in phone_number_original_list]
-        return phone_number_list, phone_number_original_list
+
+        original_phone_list = [p[0] for p in phone_number_original_list]
+        clean_phone_list = [self.clean_phone_number(p) for p in original_phone_list]
+
+        self.phone, self.original_phone_text = self.get_number(original_phone_list=original_phone_list,
+                                                               clean_phone_list=clean_phone_list)
+
+        self.get_tagged_text()
+
+        return self.phone, self.original_phone_text
+
+    def get_number(self, clean_phone_list, original_phone_list):
+        phone = []
+        original_phone_text = []
+
+        for each, each_i in zip(clean_phone_list, original_phone_list):
+            detected_number = self.number_detector.detect_entity(each)[0]
+            if detected_number:
+                phone.append(detected_number[0]['value'])
+                original_phone_text.append(each_i)
+
+        return phone, original_phone_text
 
     def clean_phone_number(self, number):
         clean_regex = re.compile('[\+()\sext]+')
@@ -51,11 +63,11 @@ class PhoneDetector(BaseDetector):
             r'((?:\(?\+(\d{1,2})\)?[\s\-\.]*)?((?=[\-\d()\s\.]{9,16}'
             r'(?:\s*e?xt?\.?\s*(?:\d{1,20}))?(?:[^\d]+|$))'
             r'(?:[\d(]{1,20}(?:[\-)\s\.]*\d{1,20}){0,20}){1,20})'
-            r'(?:\s*e?xt?\.?\s*(\d{1,20}))?)')
+            r'(?:\s*e?xt?\.?\s*(\d{1,20}))?)', re.U)
         phone_number_list = phone_number_regex.findall(self.text)
         return phone_number_list
 
-    def get_tagged_text(self, phone_number_original_list):
-        for detected_text in phone_number_original_list:
+    def get_tagged_text(self):
+        for detected_text in self.original_phone_text:
             self.tagged_text = self.tagged_text.replace(detected_text, self.tag)
             self.processed_text = self.processed_text.replace(detected_text, '')
