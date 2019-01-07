90b7a263a7a0efc086bd29a8651b8d938095480f
amansrivastav17 <amans.rlx@gmail.com>
2019-01-07 15:01:42 +0530

    fix issue english number range

--- ner_v2/detectors/numeral/number_range/en/number_range_detection.py
+++ ner_v2/detectors/numeral/number_range/en/number_range_detection.py
@@ -16,13 +16,13 @@ class NumberRangeDetector(BaseNumberRangeDetector):
                                                   data_directory_path=NumberRangeDetector.data_directory_path,
                                                   unit_type=unit_type)
 
-        self.detector_preferences = [self._detect_min_num_range_with_prefix_variants,
+        self.detector_preferences = [self._detect_min_max_num_range,
+                                     self._custom_num_range_between_num_and_num
+                                     self._detect_min_num_range_with_prefix_variants,
                                      self._detect_min_num_range_with_suffix_variants,
                                      self._detect_max_num_range_with_prefix_variants,
                                      self._detect_max_num_range_with_suffix_variants,
-                                     self._detect_min_max_num_range,
-                                     self._custom_num_range_between_num_and_num
-                                     ]
+                                    ]
 
     def _custom_num_range_between_num_and_num(self, number_range_list=None, original_list=None):
         """Detects number range of text of pattern between number1 to number2
@@ -39,13 +39,14 @@ class NumberRangeDetector(BaseNumberRangeDetector):
         """
         number_range_list = number_range_list or []
         original_list = original_list or []
-        between_range_pattern = re.compile(ur'(between\s+({number}\d+)(?:\s+and|to|-)'
+        between_range_pattern = re.compile(ur'(between\s+({number}\d+)(?:\s+and|-)'
                                            ur'\s+({number}\d+))'.format(number=NUMBER_REPLACE_TEXT), re.UNICODE)
         number_range_matches = between_range_pattern.findall(self.number_tagged_processed_text)
         for match in number_range_matches:
-            number_range_list, original_list = \
-                self._get_number_range(min_part_match=match[1], max_part_match=match[2],
-                                       full_match=match[0],
-                                       number_range_list=number_range_list,
-                                       original_list=original_list)
+            number_range, original_text = self._get_number_range(min_part_match=match[1], max_part_match=match[2],
+                                                                 full_match=match[0])
+            if number_range and original_text:
+                number_range_list.append(number_range)
+                original_list.append(original_text)
+
         return number_range_list, original_list
