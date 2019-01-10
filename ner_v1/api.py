7fbbda591d7565513f9a0bc23ec431581db8288d
chiragjn <jain.chirag925@gmail.com>
2019-01-10 15:00:13 +0530

    Refactor ner_v1.api.text and related methods. Improve code documentation slightly

--- ner_v1/api.py
+++ ner_v1/api.py
@@ -4,85 +4,98 @@ import json
 from django.http import HttpResponse
 
 from chatbot_ner.config import ner_logger
-from ner_v1.chatbot.combine_detection_logic import combine_output_of_detection_logic_and_tag
-from ner_v1.chatbot.entity_detection import get_location, get_phone_number, get_email, get_city, get_pnr, \
-    get_number, get_passenger_count, get_shopping_size, get_time, get_time_with_range, get_date, get_budget, \
-    get_person_name, get_regex
-from ner_v1.chatbot.tag_message import run_ner
-from ner_constants import PARAMETER_MESSAGE, PARAMETER_ENTITY_NAME, PARAMETER_STRUCTURED_VALUE, \
-    PARAMETER_FALLBACK_VALUE, PARAMETER_BOT_MESSAGE, PARAMETER_TIMEZONE, PARAMETER_REGEX, PARAMETER_LANGUAGE_SCRIPT, \
-    PARAMETER_SOURCE_LANGUAGE
-from ner_v1.constant import PARAMETER_MIN_TOKEN_LEN_FUZZINESS, PARAMETER_FUZZINESS, PARAMETER_MIN_DIGITS, \
-    PARAMETER_MAX_DIGITS, PARAMETER_READ_MODEL_FROM_S3, PARAMETER_READ_EMBEDDINGS_FROM_REMOTE_URL, PARAMETER_LIVE_CRF_MODEL_PATH
-from ner_v1.detectors.textual.text.text_detection_model import TextModelDetector
 from language_utilities.constant import ENGLISH_LANG
+from ner_constants import (PARAMETER_MESSAGE, PARAMETER_ENTITY_NAME, PARAMETER_STRUCTURED_VALUE,
+                           PARAMETER_FALLBACK_VALUE, PARAMETER_BOT_MESSAGE, PARAMETER_TIMEZONE, PARAMETER_REGEX,
+                           PARAMETER_LANGUAGE_SCRIPT,
+                           PARAMETER_SOURCE_LANGUAGE)
+from ner_v1.chatbot.combine_detection_logic import combine_output_of_detection_logic_and_tag
+from ner_v1.chatbot.entity_detection import (get_location, get_phone_number, get_email, get_city, get_pnr,
+                                             get_number, get_passenger_count, get_shopping_size, get_time,
+                                             get_time_with_range, get_date, get_budget,
+                                             get_person_name, get_regex, get_text)
+from ner_v1.chatbot.tag_message import run_ner
+from ner_v1.constant import (PARAMETER_MIN_TOKEN_LEN_FUZZINESS, PARAMETER_FUZZINESS, PARAMETER_MIN_DIGITS,
+                             PARAMETER_MAX_DIGITS, PARAMETER_READ_MODEL_FROM_S3,
+                             PARAMETER_READ_EMBEDDINGS_FROM_REMOTE_URL,
+                             PARAMETER_LIVE_CRF_MODEL_PATH)
+
+
+def to_bool(value):
+    # type: (Union[str, unicode, bool, int, None]) -> bool
+    if isinstance(value, bool):
+        return value
+
+    if not value:
+        return False
+
+    if isinstance(value, (int, float)):
+        return bool(value)
+
+    if isinstance(value, (str, unicode,)):
+        return value.lower() == 'true'
+
+    return False
 
 
 def get_parameters_dictionary(request):
-    """Returns the list of parameters require for NER
-    
-        Params
-            request (HttpResponse)
-                HTTP response from url
-        Returns
-           parameters_dict (json)
-                parameter dictionary
+    # type: (django.http.HttpRequest) -> Dict[str, Any]
     """
-    parameters_dict = {PARAMETER_MESSAGE: request.GET.get('message'),
-                       PARAMETER_ENTITY_NAME: request.GET.get('entity_name'),
-                       PARAMETER_STRUCTURED_VALUE: request.GET.get('structured_value'),
-                       PARAMETER_FALLBACK_VALUE: request.GET.get('fallback_value'),
-                       PARAMETER_BOT_MESSAGE: request.GET.get('bot_message'),
-                       PARAMETER_TIMEZONE: request.GET.get('timezone'),
-                       PARAMETER_REGEX: request.GET.get('regex'),
-                       PARAMETER_LANGUAGE_SCRIPT: request.GET.get('language_script', ENGLISH_LANG),
-                       PARAMETER_SOURCE_LANGUAGE: request.GET.get('source_language', ENGLISH_LANG),
-                       PARAMETER_FUZZINESS: request.GET.get('fuzziness'),
-                       PARAMETER_MIN_TOKEN_LEN_FUZZINESS: request.GET.get('min_token_len_fuzziness'),
-                       PARAMETER_MIN_DIGITS: request.GET.get('min_number_digits'),
-                       PARAMETER_MAX_DIGITS: request.GET.get('max_number_digits'),
-                       PARAMETER_READ_EMBEDDINGS_FROM_REMOTE_URL:
-                           request.GET.get('read_embeddings_from_remote_url', 'False'),
-                       PARAMETER_READ_MODEL_FROM_S3: request.GET.get('read_model_from_s3', 'False'),
-                       PARAMETER_LIVE_CRF_MODEL_PATH: request.GET.get('live_crf_model_path')
-                       }
+    Extract GET parameters from HTTP request
+
+    Args:
+        request (django.http.HttpRequest): HTTP response from url
+
+    Returns:
+       dict: GET parameters from the request
+    """
+    parameters_dict = {
+        PARAMETER_MESSAGE: request.GET.get('message'),
+        PARAMETER_ENTITY_NAME: request.GET.get('entity_name'),
+        PARAMETER_STRUCTURED_VALUE: request.GET.get('structured_value'),
+        PARAMETER_FALLBACK_VALUE: request.GET.get('fallback_value'),
+        PARAMETER_BOT_MESSAGE: request.GET.get('bot_message'),
+        PARAMETER_TIMEZONE: request.GET.get('timezone'),
+        PARAMETER_REGEX: request.GET.get('regex'),
+        PARAMETER_LANGUAGE_SCRIPT: request.GET.get('language_script', ENGLISH_LANG),
+        PARAMETER_SOURCE_LANGUAGE: request.GET.get('source_language', ENGLISH_LANG),
+        PARAMETER_FUZZINESS: request.GET.get('fuzziness'),
+        PARAMETER_MIN_TOKEN_LEN_FUZZINESS: request.GET.get('min_token_len_fuzziness'),
+        PARAMETER_MIN_DIGITS: request.GET.get('min_number_digits'),
+        PARAMETER_MAX_DIGITS: request.GET.get('max_number_digits'),
+        PARAMETER_READ_EMBEDDINGS_FROM_REMOTE_URL: to_bool(request.GET.get('read_embeddings_from_remote_url')),
+        PARAMETER_READ_MODEL_FROM_S3: to_bool(request.GET.get('read_model_from_s3')),
+        PARAMETER_LIVE_CRF_MODEL_PATH: request.GET.get('live_crf_model_path')
+    }
 
     return parameters_dict
 
 
 def text(request):
-    """This functionality initializes text detection functionality to detect textual entities.
+    """
+    Run text detector with crf model on the 'message' passed in the request
 
-    Attributes:
-        request: url parameters
+    Args:
+        request (django.http.HttpRequest): HTTP response from url
 
+    Returns:
+       dict: GET parameters from the request
     """
     try:
         parameters_dict = get_parameters_dictionary(request)
         ner_logger.debug('Start: %s ' % parameters_dict[PARAMETER_ENTITY_NAME])
-        fuzziness = parameters_dict[PARAMETER_FUZZINESS]
-        min_token_len_fuzziness = parameters_dict[PARAMETER_MIN_TOKEN_LEN_FUZZINESS]
-        read_model_from_s3 = json.loads(parameters_dict[PARAMETER_READ_MODEL_FROM_S3].lower())
-        read_embeddings_from_remote_url = json.loads(parameters_dict[PARAMETER_READ_EMBEDDINGS_FROM_REMOTE_URL].lower())
-        text_model_detector = TextModelDetector(entity_name=parameters_dict[PARAMETER_ENTITY_NAME],
-                                                source_language_script=parameters_dict[PARAMETER_LANGUAGE_SCRIPT],
-                                                read_model_from_s3=read_model_from_s3,
-                                                read_embeddings_from_remote_url=read_embeddings_from_remote_url,
-                                                live_crf_model_path=parameters_dict[PARAMETER_LIVE_CRF_MODEL_PATH]
-                                                )
-        ner_logger.debug('fuzziness: %s min_token_len_fuzziness %s' % (str(fuzziness), str(min_token_len_fuzziness)))
-        if fuzziness:
-            fuzziness = parse_fuzziness_parameter(fuzziness)
-            text_model_detector.set_fuzziness_threshold(fuzziness)
-
-        if min_token_len_fuzziness:
-            min_token_len_fuzziness = int(min_token_len_fuzziness)
-            text_model_detector.set_min_token_size_for_levenshtein(min_size=min_token_len_fuzziness)
-
-        entity_output = text_model_detector.detect(message=parameters_dict[PARAMETER_MESSAGE],
-                                                   structured_value=parameters_dict[PARAMETER_STRUCTURED_VALUE],
-                                                   fallback_value=parameters_dict[PARAMETER_FALLBACK_VALUE],
-                                                   bot_message=parameters_dict[PARAMETER_BOT_MESSAGE])
+        entity_output = get_text(
+            message=parameters_dict[PARAMETER_MESSAGE],
+            entity_name=parameters_dict[PARAMETER_ENTITY_NAME],
+            structured_value=parameters_dict[PARAMETER_STRUCTURED_VALUE],
+            fallback_value=parameters_dict[PARAMETER_FALLBACK_VALUE],
+            bot_message=parameters_dict[PARAMETER_BOT_MESSAGE],
+            fuzziness=parameters_dict[PARAMETER_FUZZINESS],
+            min_token_len_fuzziness=parameters_dict[PARAMETER_MIN_TOKEN_LEN_FUZZINESS],
+            live_crf_model_path=parameters_dict[PARAMETER_LIVE_CRF_MODEL_PATH],
+            read_model_from_s3=parameters_dict[PARAMETER_READ_MODEL_FROM_S3],
+            read_embeddings_from_remote_url=parameters_dict[PARAMETER_READ_EMBEDDINGS_FROM_REMOTE_URL],
+        )
         ner_logger.debug('Finished %s : %s ' % (parameters_dict[PARAMETER_ENTITY_NAME], entity_output))
     except TypeError as e:
         ner_logger.exception('Exception for text_synonym: %s ' % e)
@@ -453,38 +466,3 @@ def combine_output(request):
     output = combine_output_of_detection_logic_and_tag(entity_data=entity_data_json, text=message)
     ner_logger.debug('Finished %s : %s ' % (message, output))
     return HttpResponse(json.dumps({'data': output}), content_type='application/json')
-
-
-def parse_fuzziness_parameter(fuzziness):
-    """
-    This function takes input as the fuzziness value.
-    If the fuzziness is int it is returned as it is.
-    If the input is a ',' separated str value, the function returns a tuple with all values
-    present in the str after casting them to int.
-    Args:
-        fuzziness (str) or (int): The fuzzines value that needs to be parsed.
-    Returns:
-        fuzziness (tuple) or (int): It returns a tuple with of all the values cast to int present
-        in the str.
-        If the fuzziness is a single element then int is returned
-
-    Examples:
-        fuzziness = 2
-        parse_fuzziness_parameter(fuzziness)
-        >> 2
-
-        fuzziness = '3,4'
-        parse_fuzziness_parameter(fuzziness)
-        >> (3,4)
-    """
-    try:
-        if isinstance(fuzziness, int):
-            return fuzziness
-        fuzziness_split = fuzziness.split(',')
-        if len(fuzziness_split) == 1:
-            fuzziness = int(fuzziness)
-        else:
-            fuzziness = tuple([int(value.strip()) for value in fuzziness_split])
-    except ValueError as e:
-        fuzziness = 1
-    return fuzziness
