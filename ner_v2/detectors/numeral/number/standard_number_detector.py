6b9d2aeb554f031ab6de0443c3b57a2d4b86cd74
amansrivastav17 <amans.rlx@gmail.com>
2019-04-02 12:02:16 +0530

    removed unnecessary brackets

--- ner_v2/detectors/numeral/number/standard_number_detector.py
+++ ner_v2/detectors/numeral/number/standard_number_detector.py
@@ -151,8 +151,8 @@ class BaseNumberDetector(object):
 
         # add re.escape to handle decimal cases in detected original
         detected_original = re.escape(detected_original)
-        unit_matches = re.search(r'[\W]+((' + self.unit_choices + r')[\.\,\s]*' + detected_original + r')|(' +
-                                 detected_original + r'\s*(' + self.unit_choices + r'))[\W]+', processed_text,
+        unit_matches = re.search(r'\W+((' + self.unit_choices + r')[\.\,\s]*' + detected_original + r')|(' +
+                                 detected_original + r'\s*(' + self.unit_choices + r'))\W+', processed_text,
                                  re.UNICODE)
         if unit_matches:
             original_text_prefix, unit_prefix, original_text_suffix, unit_suffix = unit_matches.groups()
