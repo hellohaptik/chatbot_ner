873c01b53749ebd7ead475e0fc0e40bd17f37e58
ashutoshsingh0223 <ashutosh.singh.de@gmail.com>
2019-05-10 16:43:02 +0530

    edit in tim  tests id = en_26

--- ner_v2/tests/numeral/number_range/test_number_range_detection.py
+++ ner_v2/tests/numeral/number_range/test_number_range_detection.py
@@ -61,3 +61,95 @@ class NumberRangeDetectorTest(TestCase):
 
                 for detected_number_range in zip(detected_entities_values_list, detected_original_texts_list):
                     self.assertIn(detected_number_range, expected_zipped)
+
+
+from __future__ import absolute_import
+
+import io
+import os
+
+import pytz
+import six
+import yaml
+from django.test import TestCase
+
+from ner_v2.detectors.numeral.number_range.number_range_detection import NumberRangeDetector
+
+
+class TimeDetectionTestsMeta(type):
+    yaml_test_files = [
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "number_range_ner_tests.yaml")
+    ]
+
+    def __new__(cls, name, bases, attrs):
+        for test_name, test_fn in cls.yaml_testsuite_generator():
+            attrs[test_name] = test_fn
+
+        return super(TimeDetectionTestsMeta, cls).__new__(cls, name, bases, attrs)
+
+    @classmethod
+    def yaml_testsuite_generator(cls):
+        for filepath in cls.yaml_test_files:
+            test_data = yaml.load(io.open(filepath, "r", encoding="utf-8"))
+            for language in test_data["tests"]:
+                for i, testcase in enumerate(test_data["tests"][language]):
+                    yield (
+                        "test_yaml_{}_{}".format(language, i),
+                        cls.get_yaml_test(testcase=testcase,
+                                          language=language,)
+                    )
+
+    @classmethod
+    def get_yaml_test(cls, testcase, language, timezone, **kwargs):
+        def parse_expected_outputs(expected_outputs):
+            time_dicts, original_texts = [], []
+            for expected_output in expected_outputs:
+                time_dict = {
+                        "unit_type": expected_output["hh"],
+                        "max_value": expected_output["mm"],
+                        "unit": expected_output["nn"],
+                        "max_value": expected_output["range"],
+                        "time_type": expected_output["time_type"]
+                }
+                original_text = expected_output["original_text"].lower() if expected_output["original_text"] else None
+                if original_text:
+                    time_dicts.append(time_dict)
+                    original_texts.append(original_text)
+            return time_dicts, original_texts
+
+        failure_string_prefix = u"Test failed for\nText = {message}\nLanguage = {language}\n"
+
+        def run_test(self):
+            message = testcase["message"]
+            range_enabled = testcase.get("range_enabled")
+            time_detector = TimeDetector(entity_name='time', language=language, timezone=timezone)
+
+            time_dicts, spans = time_detector.detect_entity(text=message, range_enabled=range_enabled, **kwargs)
+
+            for time_dict in time_dicts:
+                if "range" not in time_dict:
+                    time_dict.update({"range": None})
+                if "time_type" not in time_dict:
+                    time_dict.update({"time_type": None})
+
+            expected_date_dicts, expected_spans = parse_expected_outputs(testcase["outputs"])
+            expected_outputs = list(six.moves.zip(expected_date_dicts, expected_spans))
+
+            prefix = failure_string_prefix.format(message=message, language=language)
+
+            self.assertEqual(len(time_dicts), len(spans),
+                             prefix + u"Returned dates and original_texts have different lengths")
+            self.assertEqual(len(spans), len(expected_outputs),
+                             prefix + u"Returned dates and expected_outputs have different lengths")
+
+            for output in six.moves.zip(time_dicts, spans):
+
+                self.assertIn(output, expected_outputs,
+                              prefix + u"{got} not in {expected_outputs}".format(got=output,
+                                                                                 expected_outputs=expected_outputs))
+
+        return run_test
+
+
+class TimeDetectionTests(six.with_metaclass(TimeDetectionTestsMeta, TestCase)):
+    pass
