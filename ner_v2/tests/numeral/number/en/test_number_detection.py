8acad0c03ccbd9254b16ba247112d5f051f7e494
ashutoshsingh0223 <ashutosh.singh.de@gmail.com>
2019-05-14 14:46:54 +0530

    number and phone-number test cases

--- ner_v2/tests/numeral/number/en/test_number_detection.py
+++ ner_v2/tests/numeral/number/en/test_number_detection.py
@@ -1,254 +1,84 @@
 from __future__ import absolute_import
-
+import io
+import os
+import six
+import yaml
 from django.test import TestCase
 
-
-from ner_v2.detectors.numeral.utils import get_number_from_number_word
 from ner_v2.detectors.numeral.number.number_detection import NumberDetector
-from ner_v2.detectors.numeral.number.standard_number_detector import NumberVariant
 
 
-class NumberDetectionTest(TestCase):
-    def setUp(self):
-        self.entity_name = 'number'
-        self.number_words_map = {
-            u'c': NumberVariant(scale=10000000, increment=0),
-            u'cr': NumberVariant(scale=10000000, increment=0),
-            u'crore': NumberVariant(scale=10000000, increment=0),
-            u'crores': NumberVariant(scale=10000000, increment=0),
-            u'eight': NumberVariant(scale=1, increment=8),
-            u'eighteen': NumberVariant(scale=1, increment=18),
-            u'eighty': NumberVariant(scale=1, increment=80),
-            u'eleven': NumberVariant(scale=1, increment=11),
-            u'fifteen': NumberVariant(scale=1, increment=15),
-            u'fifty': NumberVariant(scale=1, increment=50),
-            u'five': NumberVariant(scale=1, increment=5),
-            u'forty': NumberVariant(scale=1, increment=40),
-            u'four': NumberVariant(scale=1, increment=4),
-            u'fourteen': NumberVariant(scale=1, increment=14),
-            u'fourty': NumberVariant(scale=1, increment=40),
-            u'hundred': NumberVariant(scale=100, increment=0),
-            u'k': NumberVariant(scale=1000, increment=0),
-            u'l': NumberVariant(scale=100000, increment=0),
-            u'lac': NumberVariant(scale=100000, increment=0),
-            u'lacs': NumberVariant(scale=100000, increment=0),
-            u'lakh': NumberVariant(scale=100000, increment=0),
-            u'lakhs': NumberVariant(scale=100000, increment=0),
-            u'm': NumberVariant(scale=1000000, increment=0),
-            u'mil': NumberVariant(scale=1000000, increment=0),
-            u'million': NumberVariant(scale=1000000, increment=0),
-            u'nine': NumberVariant(scale=1, increment=9),
-            u'nineteen': NumberVariant(scale=1, increment=19),
-            u'ninety': NumberVariant(scale=1, increment=90),
-            u'ninty': NumberVariant(scale=1, increment=90),
-            u'one': NumberVariant(scale=1, increment=1),
-            u'seven': NumberVariant(scale=1, increment=7),
-            u'seventeen': NumberVariant(scale=1, increment=17),
-            u'seventy': NumberVariant(scale=1, increment=70),
-            u'six': NumberVariant(scale=1, increment=6),
-            u'sixteen': NumberVariant(scale=1, increment=16),
-            u'sixty': NumberVariant(scale=1, increment=60),
-            u'ten': NumberVariant(scale=1, increment=10),
-            u'thirteen': NumberVariant(scale=1, increment=13),
-            u'thirty': NumberVariant(scale=1, increment=30),
-            u'thousand': NumberVariant(scale=1000, increment=0),
-            u'thousands': NumberVariant(scale=1000, increment=0),
-            u'three': NumberVariant(scale=1, increment=3),
-            u'tweleve': NumberVariant(scale=1, increment=12),
-            u'twelve': NumberVariant(scale=1, increment=12),
-            u'twenty': NumberVariant(scale=1, increment=20),
-            u'two': NumberVariant(scale=1, increment=2),
-            u'zero': NumberVariant(scale=1, increment=0)
-        }
+class NumberDetectorTestsMeta(type):
+    yaml_test_files = [
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "number_ner_tests.yaml")
+    ]
 
-    def test_get_number_with_only_unit_in_number_word(self):
-        """
-        Number detection from word with only unit ex - 'one', 'two', 'twenty'
-        """
-        message = 'I want to book for one passenger'
-        detect_texts, original_texts = get_number_from_number_word(message, self.number_words_map)
-        zipped = zip(detect_texts, original_texts)
-        self.assertEqual(len(zipped), 1)
-        self.assertIn((1, 'one'), zipped)
+    def __new__(cls, name, bases, attrs):
+        for test_name, test_fn in cls.yaml_testsuite_generator():
+            attrs[test_name] = test_fn
 
-    def test_get_number_with_only_scale_in_number_word(self):
-        """
-        Number detection from word with only scale like - 'hundred', 'thousand'
-        """
-        message = 'need hundred change'
-        detect_texts, original_texts = get_number_from_number_word(message, self.number_words_map)
-        zipped = zip(detect_texts, original_texts)
-        self.assertEqual(len(zipped), 1)
-        self.assertIn((100, 'hundred'), zipped)
+        return super(NumberDetectorTestsMeta, cls).__new__(cls, name, bases, attrs)
 
-    def test_get_number_with_scale_and_unit_in_number_word(self):
-        """
-        Number detection from word with scale and unit like - 'one hundred', 'one thousand two hundred five'
-        """
-        message = 'haptik get one thousand two hundred five messages daily'
-        detect_texts, original_texts = get_number_from_number_word(message, self.number_words_map)
-        zipped = zip(detect_texts, original_texts)
-        self.assertEqual(len(zipped), 1)
-        self.assertIn((1205, 'one thousand two hundred five'), zipped)
+    @classmethod
+    def yaml_testsuite_generator(cls):
+        for filepath in cls.yaml_test_files:
+            test_data = yaml.load(io.open(filepath, "r", encoding="utf-8"))
+            for language in test_data["tests"]:
+                for i, testcase in enumerate(test_data["tests"][language]):
+                    yield (
+                        "test_yaml_{}".format(testcase["_id_"]),
+                        cls.get_yaml_test(testcase=testcase,
+                                          language=language,)
+                    )
 
-    def test_get_number_with_multiple_unit_number_word(self):
-        """
-        Number detection from word with multiple unit numbers like 'one two three twenty one'
-        """
-        message = 'one two three'
-        detect_texts, original_texts = get_number_from_number_word(message, self.number_words_map)
-        zipped = zip(detect_texts, original_texts)
-        self.assertEqual(len(zipped), 3)
-        self.assertIn((1, 'one'), zipped)
-        self.assertIn((2, 'two'), zipped)
-        self.assertIn((3, 'three'), zipped)
+    @classmethod
+    def get_yaml_test(cls, testcase, language, **kwargs):
+        def parse_expected_outputs(expected_outputs):
+            num_dicts, original_texts = [], []
+            for expected_output in expected_outputs:
+                if "detected_number" in expected_output:
+                    num_dict = {
+                        "detected_number": expected_output["detected_number"]
+                    }
 
-    def test_get_number_with_multiple_unit_scale_number_word(self):
-        """
-        Number detection from word with multiple unit scale numbers like 'one thousand to one hundred two'
-        """
-        message = 'one thousand to one hundred two'
-        detect_texts, original_texts = get_number_from_number_word(message, self.number_words_map)
-        zipped = zip(detect_texts, original_texts)
-        self.assertEqual(len(zipped), 2)
-        self.assertIn((1000, 'one thousand'), zipped)
-        self.assertIn((102, 'one hundred two'), zipped)
+                else:
+                    num_dict = {
+                        "value": expected_output["value"],
+                        "unit": expected_output["unit"],
+                    }
+                original_text = \
+                    expected_output["original_text"].lower().strip() if expected_output["original_text"] else None
+                if original_text:
+                    num_dicts.append(num_dict)
+                    original_texts.append(original_text)
+            return num_dicts, original_texts
 
-    def test_get_number_with_combination_of_unit_scale_and_unit_number_word(self):
-        """
-        Number detection from word with combination of unit scale and unit number like 'one thousand one two three'
-        """
-        message = 'one thousand one two three'
-        detect_texts, original_texts = get_number_from_number_word(message, self.number_words_map)
-        zipped = zip(detect_texts, original_texts)
-        self.assertEqual(len(zipped), 3)
-        self.assertIn((1001, 'one thousand one'), zipped)
-        self.assertIn((2, 'two'), zipped)
-        self.assertIn((3, 'three'), zipped)
+        failure_string_prefix = u"Test failed for\nText = {message}\nLanguage = {language}\n"
 
-    def test_get_number_with_multiple_spaces_in_unit_scale_number_word(self):
-        """
-        Number detection from word with multiple spaces in unit scale number like 'one thousand   one   hundred two'
-        """
-        message = 'there are one thousand   one   hundred two students attending placement drive'
-        detect_texts, original_texts = get_number_from_number_word(message, self.number_words_map)
-        zipped = zip(detect_texts, original_texts)
-        self.assertEqual(len(zipped), 1)
-        self.assertIn((1102, 'one thousand   one   hundred two'), zipped)
+        def run_test(self):
+            message = testcase["message"]
+            unit_type = testcase.get("unit_type", None)
+            number_detector_object = NumberDetector(entity_name="number", language=language, unit_type=unit_type)
+            number_dicts, spans = number_detector_object.detect_entity(message)
 
-    def test_en_number_detection_for_integer_number(self):
-        """
-        Number detection for english language for integer number like '100', '2'
-        """
-        message = u'100 got selected for interview'
-        number_detector_object = NumberDetector(entity_name=self.entity_name, language='en')
-        number_dicts, original_texts = number_detector_object.detect_entity(message)
-        zipped = zip(number_dicts, original_texts)
-        self.assertEqual(len(zipped), 1)
-        self.assertIn(({'value': '100', 'unit': None}, u'100'), zipped)
+            expected_number_dicts, expected_spans = parse_expected_outputs(testcase["outputs"])
+            expected_outputs = list(six.moves.zip(expected_number_dicts, expected_spans))
 
-    def test_en_number_detection_for_integer_number_with_unit_and_unit_type_given(self):
-        """
-        Number detection for english language for integer number with units like 'Rs100', '2Rs'
-        """
-        message = u'rs.100 is the application charger'
-        number_detector_object = NumberDetector(entity_name=self.entity_name, language='en', unit_type='currency')
-        number_dicts, original_texts = number_detector_object.detect_entity(message)
-        zipped = zip(number_dicts, original_texts)
-        self.assertEqual(len(zipped), 1)
-        self.assertIn(({'value': '100', 'unit': 'rupees'}, 'rs.100'), zipped)
+            prefix = failure_string_prefix.format(message=message, language=language)
 
-    def test_en_number_detection_for_decimal_number(self):
-        """
-        Number detection for english language for decimal number like '100.2'
-        """
-        message = u'Todays temperature is 11.2 degree celsius'
-        number_detector_object = NumberDetector(entity_name=self.entity_name, language='en')
-        number_dicts, original_texts = number_detector_object.detect_entity(message)
+            self.assertEqual(len(number_dicts), len(spans),
+                             prefix + u"Returned dates and original_texts have different lengths")
+            self.assertEqual(len(spans), len(expected_outputs),
+                             prefix + u"Returned dates and expected_outputs have different lengths")
 
-        zipped = zip(number_dicts, original_texts)
-        self.assertEqual(len(zipped), 1)
-        self.assertIn(({'value': '11.2', 'unit': None}, u'11.2'), zipped)
+            for output in six.moves.zip(number_dicts, spans):
 
-    def test_en_number_detection_for_decimal_number_with_unit_and_unit_type_given(self):
-        """
-        Number detection for english language for decimal number with unit like '10.2k rupees'
-        """
-        message = u'my monthly salary is 10.12k rupees'
-        number_detector_object = NumberDetector(entity_name=self.entity_name, language='en', unit_type='currency')
-        number_dicts, original_texts = number_detector_object.detect_entity(message)
+                self.assertIn(output, expected_outputs,
+                              prefix + u"{got} not in {expected_outputs}".format(got=output,
+                                                                                 expected_outputs=expected_outputs))
 
-        zipped = zip(number_dicts, original_texts)
-        self.assertEqual(len(zipped), 1)
-        self.assertIn(({'value': '10120', 'unit': 'rupees'}, u'10.12k rupees'), zipped)
+        return run_test
 
-    def test_en_number_detection_for_integer_number_with_scale(self):
-        """
-        Number detection for english language for integer number with scale like '1 thousand', '1k', '1m'
-        """
-        message = '1 thousand men were killed in war'
-        number_detector_object = NumberDetector(entity_name=self.entity_name, language='en')
-        number_dicts, original_texts = number_detector_object.detect_entity(message)
 
-        zipped = zip(number_dicts, original_texts)
-        self.assertEqual(len(zipped), 1)
-        self.assertIn(({'value': '1000', 'unit': None}, u'1 thousand'), zipped)
-
-    def test_en_number_detection_for_integer_number_with_scale_and_unit_and_unit_type_given(self):
-        """
-        Number detection for english language for integer number with scale and unit like 'Rs 1 thousand', '1k Rs'
-        """
-        message = 'i need 1 thousand rupees'
-        number_detector_object = NumberDetector(entity_name=self.entity_name, language='en', unit_type='currency')
-        number_dicts, original_texts = number_detector_object.detect_entity(message)
-
-        zipped = zip(number_dicts, original_texts)
-        self.assertEqual(len(zipped), 1)
-        self.assertIn(({'value': '1000', 'unit': 'rupees'}, u'1 thousand rupees'), zipped)
-
-    def test_en_number_detection_for_decimal_number_with_scale(self):
-        """
-        Number detection for english language for decimal number with scale like '1.2 thousand', '2.2k', '1.4m'
-        """
-        message = 'my monthly salary is 2.2k'
-        number_detector_object = NumberDetector(entity_name=self.entity_name, language='en')
-        number_dicts, original_texts = number_detector_object.detect_entity(message)
-
-        zipped = zip(number_dicts, original_texts)
-        self.assertEqual(len(zipped), 1)
-        self.assertIn(({'value': '2200', 'unit': None}, u'2.2k'), zipped)
-
-    def test_en_number_detection_for_decimal_number_with_scale_and_unit(self):
-        """
-        Number detection for english language for decimal number with scale like '1.2 thousand', '2.2k' excluding unit
-        """
-        message = 'I bought a car toy for 2.3k rupees'
-        number_detector_object = NumberDetector(entity_name=self.entity_name, language='en')
-        number_dicts, original_texts = number_detector_object.detect_entity(message)
-
-        zipped = list(zip(number_dicts, original_texts))
-        self.assertEqual(len(zipped), 1)
-        self.assertIn(({'value': '2300', 'unit': None}, u'2.3k'), zipped)
-
-    def test_en_number_detection_for_decimal_number_with_scale_and_unit_and_unit_type_given(self):
-        """
-        Number detection for english language for decimal number with scale like '1.2 thousand rupees', 'Rupees 2.2k'
-        """
-        message = 'I bought a car toy for 2.3k rupees'
-        number_detector_object = NumberDetector(entity_name=self.entity_name, language='en', unit_type='currency')
-        number_dicts, original_texts = number_detector_object.detect_entity(message)
-
-        zipped = zip(number_dicts, original_texts)
-        self.assertEqual(len(zipped), 1)
-        self.assertIn(({'value': '2300', 'unit': 'rupees'}, u'2.3k rupees'), zipped)
-
-    def test_en_number_detection_for_decimal_number_with_scale_and_unit_and_different_unit_type_given(self):
-        """
-        Number detection for english language for decimal number with scale like '1.2 thousand rupees', 'Rupees 2.2k'
-        """
-        message = 'I buys 2.3k kg mango'
-        number_detector_object = NumberDetector(entity_name=self.entity_name, language='en', unit_type='currency')
-        number_dicts, original_texts = number_detector_object.detect_entity(message)
-
-        zipped = list(zip(number_dicts, original_texts))
-        self.assertEqual(len(zipped), 0)
+class NumberDetectorTests(six.with_metaclass(NumberDetectorTestsMeta, TestCase)):
+    pass
--- ner_v2/tests/pattern/phone_number/test_phone_number_detection.py
+++ ner_v2/tests/pattern/phone_number/test_phone_number_detection.py
@@ -1,46 +1,74 @@
 from __future__ import absolute_import
-
+import io
 import os
-import pandas as pd
+import six
+import yaml
 from django.test import TestCase
 
 from ner_v2.detectors.pattern.phone_number.phone_number_detection import PhoneDetector
 
 
-class PhoneDetectionTest(TestCase):
-    def setUp(self):
-        self.phone_number_detection = PhoneDetector(entity_name='phone_number')
-        self.csv_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                                     'data',
-                                     'phone_detection_test_cases.csv')
-        self.data = pd.read_csv(self.csv_path, encoding='utf-8')
-        self.test_dict = self.preprocess_test_cases()
+class PhoneNumberDetectorTestsMeta(type):
+    yaml_test_files = [
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "phone_number_ner_tests.yaml")
+    ]
 
-    def preprocess_test_cases(self):
-        test_dict = {
-            'language': [],
-            'message': [],
-            'expected_value': [],
-        }
-        for language, message, detected_entity, original_entity in zip(self.data['language'], self.data['message'],
-                                                                       self.data['detected_entities'],
-                                                                       self.data['original_entities']):
-            de, oe = [], []
-            de.extend(detected_entity.split('|'))
-            oe.extend(original_entity.split('|'))
-            temp = []
-            for d, o in zip(de, oe):
-                temp.append((d, o))
-            test_dict['language'].append(language)
-            test_dict['message'].append(message)
-            test_dict['expected_value'].append(temp)
+    def __new__(cls, name, bases, attrs):
+        for test_name, test_fn in cls.yaml_testsuite_generator():
+            attrs[test_name] = test_fn
 
-        return test_dict
+        return super(PhoneNumberDetectorTestsMeta, cls).__new__(cls, name, bases, attrs)
 
-    def test_phone_number_detection(self):
-        for i in range(len(self.data)):
-            message = self.test_dict['message'][i]
-            expected_value = self.test_dict['expected_value'][i]
-            detected_texts, original_texts = self.phone_number_detection.detect_entity(text=message)
-            zipped = zip(detected_texts, original_texts)
-            self.assertEqual(expected_value, zipped)
+    @classmethod
+    def yaml_testsuite_generator(cls):
+        for filepath in cls.yaml_test_files:
+            test_data = yaml.load(io.open(filepath, "r", encoding="utf-8"))
+            for language in test_data["tests"]:
+                for i, testcase in enumerate(test_data["tests"][language]):
+                    yield (
+                        "test_yaml_{}".format(testcase["_id_"]),
+                        cls.get_yaml_test(testcase=testcase,
+                                          language=language,)
+                    )
+
+    @classmethod
+    def get_yaml_test(cls, testcase, language, **kwargs):
+        def parse_expected_outputs(expected_outputs):
+            phone_num_list, original_texts = [], []
+            for expected_output in expected_outputs:
+                original_text = \
+                    expected_output["original_text"].lower().strip() if expected_output["original_text"] else None
+                if original_text:
+                    phone_num_list.append(expected_output["value"])
+                    original_texts.append(original_text)
+            return phone_num_list, original_texts
+
+        failure_string_prefix = u"Test failed for\nText = {message}\nLanguage = {language}\n"
+
+        def run_test(self):
+            message = testcase["message"]
+            number_detector_object = PhoneDetector(entity_name='phone_number', language=language)
+            phone_number_list, spans = number_detector_object.detect_entity(message)
+
+            expected_phone_number_list, expected_spans = parse_expected_outputs(testcase["outputs"])
+
+            expected_outputs = list(six.moves.zip(expected_phone_number_list, expected_spans))
+
+            prefix = failure_string_prefix.format(message=message, language=language)
+
+            self.assertEqual(len(phone_number_list), len(spans),
+                             prefix + u"Returned dates and original_texts have different lengths")
+            self.assertEqual(len(spans), len(expected_outputs),
+                             prefix + u"Returned dates and expected_outputs have different lengths")
+
+            for output in six.moves.zip(phone_number_list, spans):
+
+                self.assertIn(output, expected_outputs,
+                              prefix + u"{got} not in {expected_outputs}".format(got=output,
+                                                                                 expected_outputs=expected_outputs))
+
+        return run_test
+
+
+class PhoneNumberDetectorTests(six.with_metaclass(PhoneNumberDetectorTestsMeta, TestCase)):
+    pass
