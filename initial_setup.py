4f74aeb039ea2a1467890a19bea3d37419ce460e
Chirag Jain <jain.chirag925@gmail.com>
2019-06-27 18:10:48 +0530

    Fix typo

--- initial_setup.py
+++ initial_setup.py
@@ -8,22 +8,22 @@ BASE_DIR = os.path.dirname(__file__)
 print("Downloading nltk corpus: punkt ...")
 status = nltk.download('punkt')
 if not status:
-    print("punkt Download was unsucessful")
+    print("punkt Download was unsuccessful")
 
 print("Downloading nltk corpus: wordnet ...")
 status = nltk.download('wordnet')
 if not status:
-    print("wordnet Download was unsucessful")
+    print("wordnet Download was unsuccessful")
 
 print("Downloading nltk corpus: MaxEnt POS ...")
 status = nltk.download('maxent_treebank_pos_tagger')
 if not status:
-    print("MaxEnt POS Download was unsucessful")
+    print("MaxEnt POS Download was unsuccessful")
 
 print("Downloading nltk corpus: AP POS Tagger...")
 status = nltk.download('averaged_perceptron_tagger')
 if not status:
-    print("AP POS Tagger Download was unsucessful")
+    print("AP POS Tagger Download was unsuccessful")
 
 # Below needs to be committed if you want to use existing data in the Elasticsearch Setup
 
