2ff7282c999f696dcfb1a4a7e00313783c4c6773
ruthvik-17 <ruthvik@haptik.co>
2019-09-17 12:40:24 +0530

    added compatability with both phonenumbers and phonenumberslite

--- ner_v2/detectors/pattern/phone_number/new_phone_number_detection.py
+++ ner_v2/detectors/pattern/phone_number/new_phone_number_detection.py
@@ -74,9 +74,14 @@ class PhoneDetector(BaseDetector):
         self.text = text
         self.phone, self.original_phone_text = [], []
         for match in phonenumbers.PhoneNumberMatcher(text, self.country_code):
+            if match.number.national_number[-1:] == 'L':
+                phone_number = match.number.national_number[:-1]
+                # [:-1] is to remove the letter 'L' which is coming along with the number in phonenumberslite library.
+            else:
+                phone_number = match.number.national_number
+                # above L bug is not there in normal phonenumbers library.
             self.phone.append({"country_calling_code": match.number.country_code,
-                               "phone_number": match.number.national_number[:-1]})
-            # [:-1] is to remove the letter 'L' which is coming along with the number in phonenumberslite library.
+                               "phone_number": phone_number})
             self.original_phone_text.append(self.text[match.start:match.end])
 
         return self.phone, self.original_phone_text
