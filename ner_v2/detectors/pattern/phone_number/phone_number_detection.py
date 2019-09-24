770c06572dbae3d0851b513bbf801f74a07a0132
ruthvik-17 <ruthvik@haptik.co>
2019-09-24 17:27:37 +0530

    added method to get country code from locale

--- ner_v2/detectors/pattern/phone_number/phone_number_detection.py
+++ ner_v2/detectors/pattern/phone_number/phone_number_detection.py
@@ -35,23 +35,24 @@ class PhoneDetector(BaseDetector):
          original_phone_text (list): list to store substrings of the text detected as phone numbers
     """
 
-    def __init__(self, entity_name, language=ENGLISH_LANG, country_code="IN"):
+    def __init__(self, entity_name, language=ENGLISH_LANG, locale='en-IN'):
         # Todo: Change default from india to get it from the bot.
         """
         Args:
             entity_name (str): A string by which the detected numbers would be replaced with
             on calling detect_entity()
             language (str, optional): language code of number text, defaults to 'en'
-            country_code(str, optional): country code of the country from which you are using
+            locale(str, optional): locale of the country from which you are dialing. Ex: 'en-IN'
         """
         self._supported_languages = NumberDetector.get_supported_languages()
-        super(PhoneDetector, self).__init__(language, country_code)
+        super(PhoneDetector, self).__init__(language, locale)
         self.language = language
         self.entity_name = entity_name
+        self.locale = locale
         self.text = ''
         self.phone = []
         self.original_phone_text = []
-        self.country_code = country_code.upper()
+        self.country_code = ''
         self.number_word_dict = create_number_word_dict()
 
     @property
@@ -62,6 +63,14 @@ class PhoneDetector(BaseDetector):
              list: List of ISO 639 codes of languages supported by subclass/detector
         """
         return self._supported_languages
+
+    def get_country_code_from_locale(self):
+        """
+        This method sets self.country_code from given locale
+        """
+        regex_pattern = re.compile('[-_](.*$)', re.U)
+        match = regex_pattern.findall(self.locale)
+        self.country_code = match[0].upper()
 
     def convert_words_to_numbers(self, text):
         """
@@ -89,6 +98,14 @@ class PhoneDetector(BaseDetector):
             converted_sentence = re.sub(r'(\d+)(\s+)(\d+)', r'\1\3', converted_sentence)
         return converted_sentence
 
+    def get_correct_original_text(self, original_text):
+        pattern = ''
+        for text_match in original_text:
+            pattern += ('(^.*)' + '(?:' + text_match + ')')
+        pattern += '(.*$)'
+        first_pattern = re.compile(pattern, re.U)
+        first_matches = first_pattern.findall(self.text)
+
     def detect_entity(self, text, **kwargs):
         """Detects phone numbers in the text string
 
@@ -98,24 +115,27 @@ class PhoneDetector(BaseDetector):
 
         Returns:
 
-            self.phone (list): list consisting the detected phone numbers
+            self.phone (list): list consisting the detected phone numbers and their country calling codes
             self.original_phone_text (list): list containing their corresponding substrings in the original message.
 
         Examples:
 
-        text = 'call +1 (408) 912-6172 and send 100rs to 9920441344'
-
-        p = PhoneDetector(entity_name='phone_number', language='en')
+        text = 'call +1 (408) 912-6172'
+        p = PhoneDetector(entity_name='phone_number', language='en', locale='en-US')
         p.detect_entity(text=text)
-        (['14089126172', '9920441344'], [u'+1 (408) 912-6172', u'9920441344'])
+        ([{'country_calling_code':'1', phone_number':'4089126172'} ],
+         [u'+1 (408) 912-6172'])
 
         text = '+९१ ९८१९९८३१३२ पर कॉल करें और संदेश ९८२०३३४४१६ पर कॉल करें'
-        p = PhoneDetector(entity_name='phone_number', language='hi')
+        p = PhoneDetector(entity_name='phone_number', language='hi', locale='en-IN')
         p.detect_entity(text=text)
-        (['919819983132', '9820334416'],[u'+९१ ९८१९९८३१३२', u'+९१ ९८१९९८३१३२'])
+        ([{'country_calling_code':'91', phone_number':'9819983132'}
+        ,{ 'country_calling_code':'91', phone_number:'9820334416'} ],
+        [u'+९१ ९८१९९८३१३२', u'+९१ ९८१९९८३१३२'])
 
         """
-
+        self.get_country_code_from_locale()
+        original_text = text
         if self.language == 'en':
             self.text = self.convert_words_to_numbers(text)
         else:
@@ -125,5 +145,6 @@ class PhoneDetector(BaseDetector):
             self.phone.append({"country_calling_code": str(match.number.country_code),
                                "phone_number": str(match.number.national_number)})
             self.original_phone_text.append(self.text[match.start:match.end])
-
+        # if original_text != self.text:
+        #     self.get_correct_original_text(original_text)
         return self.phone, self.original_phone_text
