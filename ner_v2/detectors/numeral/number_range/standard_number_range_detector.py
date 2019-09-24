df720d54e5794e491bdcb5a76dcf4f4c6f4ec0fa
ruthvik-17 <ruthvik@haptik.co>
2019-09-24 18:31:31 +0530

    resolved lint errors and _detect_absolute_number

--- ner_v2/detectors/numeral/number_range/standard_number_range_detector.py
+++ ner_v2/detectors/numeral/number_range/standard_number_range_detector.py
@@ -193,10 +193,10 @@ class BaseNumberRangeDetector(object):
                 numeral_constant.NUMBER_DETECTION_RETURN_DICT_UNIT]
             if (self.unit_type and entity_unit == self.unit_type) or not self.unit_type:
                 number_list.append({numeral_constant.NUMBER_RANGE_MAX_VALUE: None,
-                                        numeral_constant.NUMBER_RANGE_MIN_VALUE: None,
-                                        numeral_constant.NUMBER_RANGE_VALUE_UNIT: entity_unit,
-                                        numeral_constant.NUMBER_RANGE_ABS_VALUE: self.number_detected_map[
-                                            match].entity_value})
+                                    numeral_constant.NUMBER_RANGE_MIN_VALUE: None,
+                                    numeral_constant.NUMBER_RANGE_VALUE_UNIT: entity_unit,
+                                    numeral_constant.NUMBER_RANGE_ABS_VALUE: self.
+                                   number_detected_map[match].entity_value.NUMBER_DETECTION_RETURN_DICT_VALUE})
                 original_list.append(self.number_detected_map[match].original_text)
         return number_list, original_list
 
