1d734afddeb0bdc89d58b220f19920ae0dbc2354
Chirag Jain <jain.chirag925@gmail.com>
2019-12-18 12:25:13 +0530

    Cleanup some conditions in config.py

--- chatbot_ner/settings.py
+++ chatbot_ner/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-_dj_debug = os.environ.get('DJANGO_DEBUG')
+_dj_debug = os.environ.get('DJANGO_DEBUG', 'false')
 DEBUG = (_dj_debug and _dj_debug.lower() == 'true')
 
 TEMPLATE_DEBUG = False
