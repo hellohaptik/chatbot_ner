d7198e7d8516c7b29ccf625c73b1e6bb9ec805f2
Raza Sayed <raza.sayed@gmail.com>
2020-03-23 11:10:07 +0000

    Fix lint errors

--- ner_v2/tests/postman/run_v2_tests.py
+++ ner_v2/tests/postman/run_v2_tests.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 import json
-import traceback
 import os
 
 
@@ -15,21 +14,22 @@ def check_if_data_valid(entities):
     # Check if all data files are valid json and there are same number of records in all of them
     # Also check that every test case has the keys: input and expected
     try:
-       number_of_records = get_number_of_records_per_entity()
-       for entity in entities:
-           file_name = 'data/' + entity + '.json'
-           with open(file_name, 'r') as file:
-               data = json.load(file)
-           if (len(data) != number_of_records):
-               print('Incorrect number of records in ' + file_name)
-               return False
-           for item in data:
-               if not all (k in item for k in ('input', 'expected')):
-                   print('Invalid json data in ' + file_name)
-                   return False
-       return True
+        number_of_records = get_number_of_records_per_entity()
+        for entity in entities:
+            file_name = 'data/' + entity + '.json'
+            with open(file_name, 'r') as file:
+                data = json.load(file)
+            if len(data) != number_of_records:
+                print('Incorrect number of records in ' + file_name)
+                return False
+            for item in data:
+                if not all(k in item for k in ('input', 'expected')):
+                    print('Invalid json data in ' + file_name)
+                    return False
+        return True
     except Exception as e:
-        print('Invalid json data in ' + file_name + '\n' + traceback.format_exc())
+        print('Invalid json data in ' + file_name
+              + '\n' + str(e))
         return False
 
 
@@ -55,32 +55,25 @@ def generate_newman_data(entities_data):
     number_of_records = get_number_of_records_per_entity()
     result = []
     for i in range(number_of_records):
-        l = []
+        entities_list = []
         d = {}
         for k in entities_data:
-            l.append(entities_data[k][i])
-        for item in l:
+            entities_list.append(entities_data[k][i])
+        for item in entities_list:
             d.update(item)
         result.append(d)
     return result
-    
+
 
 entities = ['number_range', 'phoneV2']
 
-if (check_if_data_valid(entities)):
+
+if check_if_data_valid(entities):
     try:
         entities_data = read_entities_data(entities)
         newman_data = generate_newman_data(entities_data)
         with open('newman_v2_data.json', 'w') as fp:
-            json.dump(newman_data, fp,  indent=4)
+            json.dump(newman_data, fp, indent=4)
         os.system('newman run ner_v2_collection.json -d newman_v2_data.json -e dev.json')
     except Exception as e:
-        print(traceback.format_exc())
-
-
-
-
-
-
-
-    
+        print(str(e))
