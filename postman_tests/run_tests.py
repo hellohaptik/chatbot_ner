529fd39ad9ae965755914f23e1a6da73c0c06f08
Raza Sayed <raza.sayed@gmail.com>
2020-04-10 16:00:23 +0530

    Passing newman return code to parent process

--- postman_tests/run_tests.py
+++ postman_tests/run_tests.py
@@ -1,20 +1,27 @@
 from __future__ import absolute_import
-from subprocess import Popen,PIPE
+from subprocess import Popen
 import os
 import json
+import sys
 from lib import newman
 from lib import datastore
 
 
 postman_tests_directory = os.path.dirname(os.path.abspath(__file__))
-entities_data_path = os.path.join(postman_tests_directory, 'data', 'entities')
 newman_data_path = os.path.join(postman_tests_directory, 'data', 'newman_data.json')
-collection_data_path = os.path.join(postman_tests_directory, 'data', 'ner_collection.json')
-es_data_path = os.path.join(postman_tests_directory, 'data', 'elastic_search')
 config_path = os.path.join(postman_tests_directory, 'config')
 
 
 def get_newman_command():
+    """ Returns the newman shell command to be used for running the tests
+
+    Args:
+        None
+
+    Returns:
+        (str): The shell command to be used for running the tests
+    """
+    collection_data_path = os.path.join(postman_tests_directory, 'data', 'ner_collection.json')
     if os.path.exists(f"{config_path}/dev.json"):
         environment_file_path = f'{config_path}/dev.json'
         return (
@@ -29,18 +36,33 @@ def get_newman_command():
         )
 
 
-try:
-    newman.check_if_data_valid(entities_data_path)
-    datastore.sync(es_data_path, config_path, 'create')
-    newman_data = newman.generate_newman_data(entities_data_path)
-    with open(newman_data_path, 'w') as fp:
-        json.dump(newman_data, fp)
-    newman_command = get_newman_command()
-    process = Popen(newman_command, shell=True)
-    (out, err) = process.communicate()
-    print(process.returncode)
-except Exception as e:
-    raise e
-finally:
-    os.remove(newman_data_path)
-    datastore.sync(es_data_path, config_path, 'delete')
+def run_tests():
+    """ Runs the newman test-suite
+
+    Args:
+        None
+
+    Returns:
+        (str): The return code of the newman command
+    """
+    entities_data_path = os.path.join(postman_tests_directory, 'data', 'entities')
+    es_data_path = os.path.join(postman_tests_directory, 'data', 'elastic_search')
+    try:
+        newman.check_if_data_valid(entities_data_path)
+        datastore.sync(es_data_path, config_path, 'create')
+        newman_data = newman.generate_newman_data(entities_data_path)
+        with open(newman_data_path, 'w') as fp:
+            json.dump(newman_data, fp)
+        newman_command = get_newman_command()
+        process = Popen(newman_command, shell=True)
+        process.communicate()
+        os.remove(newman_data_path)
+        return process.returncode
+    except Exception as e:
+        raise e
+    finally:
+        datastore.sync(es_data_path, config_path, 'delete')
+
+if __name__ == "__main__":
+    status = run_tests()
+    sys.exit(status)
