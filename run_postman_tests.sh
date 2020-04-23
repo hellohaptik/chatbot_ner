a6bdaa7d5ea5017835e0eb31914f643619ee451c
Raza Sayed <raza.sayed@gmail.com>
2020-04-23 15:09:32 +0530

    Handling --html argument in run_postman_tests shell script

--- run_postman_tests.sh
+++ run_postman_tests.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker exec -it docker_chatbot-ner_1 python postman_tests/run_tests.py
+if [ "$1" == "--html" ]; then
+    docker exec -it docker_chatbot-ner_1 python postman_tests/run_tests.py --html
+else
+    docker exec -it docker_chatbot-ner_1 python postman_tests/run_tests.py
+fi
