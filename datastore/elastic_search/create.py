19d63f6be4f1556242531675117cdf62bedffd02
Chirag Jain <jain.chirag925@gmail.com>
2020-06-09 20:22:41 +0530

    Update alias creation and deletion code and add type hints to create.py

--- datastore/elastic_search/create.py
+++ datastore/elastic_search/create.py
@@ -1,10 +1,16 @@
+import logging
+from typing import List, Dict, Any
+
+from elasticsearch import Elasticsearch
+
 from .utils import filter_kwargs
 
 log_prefix = 'datastore.elastic_search.create'
 
 
 def exists(connection, index_name):
-    '''
+    # type: (Elasticsearch, str) -> bool
+    """
     Checks if index_name exists
 
     Args:
@@ -13,18 +19,20 @@ def exists(connection, index_name):
 
     Returns:
         boolean, True if index exists , False otherwise
-    '''
+    """
     return connection.indices.exists(index_name)
 
 
 def delete_index(connection, index_name, logger, err_if_does_not_exist=True, **kwargs):
-    '''
+    # type: (Elasticsearch, str, logging.Logger, bool, **Any) -> None
+    """
     Deletes the index named index_name
 
     Args:
         connection: Elasticsearch client object
         index_name: The name of the index
         logger: logging object to log at debug and exception level
+        err_if_does_not_exist: if to raise error if index does not exist already, defaults to True
         kwargs:
             body: The configuration for the index (settings and mappings)
             master_timeout: Specify timeout for connection to master
@@ -33,7 +41,7 @@ def delete_index(connection, index_name, logger, err_if_does_not_exist=True, **k
             wait_for_active_shards: Set the number of active shards to wait for before the operation returns.
 
         Refer https://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.client.IndicesClient.delete
-    '''
+    """
     if not exists(connection, index_name):
         if err_if_does_not_exist:
             raise Exception('Failed to delete index {}. It does not exist!'.format(index_name))
@@ -45,7 +53,8 @@ def delete_index(connection, index_name, logger, err_if_does_not_exist=True, **k
 
 
 def _create_index(connection, index_name, doc_type, logger, mapping_body, err_if_exists=True, **kwargs):
-    '''
+    # type: (Elasticsearch, str, str, logging.Logger, Dict[str, Any], bool, **Any) -> None
+    """
     Creates an Elasticsearch index needed for similarity based searching
     Args:
         connection: Elasticsearch client object
@@ -53,6 +62,7 @@ def _create_index(connection, index_name, doc_type, logger, mapping_body, err_if
         doc_type:  The type of the documents that will be indexed
         logger: logging object to log at debug and exception level
         mapping_body: dict, mappings to put on the index
+        err_if_exists: if to raise error if the index already exists, defaults to True
         kwargs:
             master_timeout: Specify timeout for connection to master
             timeout: Explicit operation timeout
@@ -67,7 +77,7 @@ def _create_index(connection, index_name, doc_type, logger, mapping_body, err_if
                                 (missing or closed)
         Refer https://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.client.IndicesClient.create
         Refer https://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.client.IndicesClient.put_mapping
-    '''
+    """
     if exists(connection=connection, index_name=index_name):
         if err_if_exists:
             raise Exception('Failed to create index {}. it already exists. Please check and delete it using '
@@ -118,7 +128,8 @@ def _create_index(connection, index_name, doc_type, logger, mapping_body, err_if
 
 
 def create_entity_index(connection, index_name, doc_type, logger, **kwargs):
-    '''
+    # type: (Elasticsearch, str, str, logging.Logger, **Any) -> None
+    """
     Creates an mapping specific to entity storage in elasticsearch and makes a call to create_index
     to create the index with the given mapping body
     Args:
@@ -141,7 +152,7 @@ def create_entity_index(connection, index_name, doc_type, logger, **kwargs):
 
         Refer https://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.client.IndicesClient.create
         Refer https://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.client.IndicesClient.put_mapping
-    '''
+    """
     mapping_body = {
         doc_type: {
             'properties': {
@@ -180,7 +191,8 @@ def create_entity_index(connection, index_name, doc_type, logger, **kwargs):
 
 
 def create_crf_index(connection, index_name, doc_type, logger, **kwargs):
-    '''
+    # type: (Elasticsearch, str, str, logging.Logger, **Any) -> None
+    """
     This method is used to create an index with mapping suited for story training_data
     Args:
         connection: Elasticsearch client object
@@ -202,7 +214,7 @@ def create_crf_index(connection, index_name, doc_type, logger, **kwargs):
 
         Refer https://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.client.IndicesClient.create
         Refer https://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.client.IndicesClient.put_mapping
-    '''
+    """
     mapping_body = {
         doc_type: {
             'properties': {
@@ -226,17 +238,36 @@ def create_crf_index(connection, index_name, doc_type, logger, **kwargs):
 
 
 def create_alias(connection, index_list, alias_name, logger, **kwargs):
-    '''
+    # type: (Elasticsearch, List[str], str, logging.Logger, **Any) -> None
+    """
     This method is used to create alias for list of indices
     Args:
-        connection:
+        connection: Elasticsearch client object
         index_list (list): List of indices the alias has to point to
         alias_name (str): Name of the alias
         logger: logging object to log at debug and exception level
 
         **kwargs:
             https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-aliases.html
-    '''
-    logger.debug('Alias creation %s started %s' % alias_name)
+    """
+    logger.debug('Putting alias %s to indices: %s' % (alias_name, str(index_list)))
     connection.indices.put_alias(index=index_list, name=alias_name, **kwargs)
     logger.debug('Alias %s now points to indices %s' % (alias_name, str(index_list)))
+
+
+def delete_alias(connection, index_list, alias_name, logger, **kwargs):
+    # type: (Elasticsearch, List[str], str, logging.Logger, **Any) -> None
+    """
+    Delete alias `alias_name` from list of indices in `index_list`
+    Args:
+        connection: Elasticsearch client object
+        index_list (list): List of indices the alias has to point to
+        alias_name (str): Name of the alias
+        logger: logging object to log at debug and exception level
+
+        **kwargs:
+            https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-aliases.html
+    """
+    logger.debug('Removing alias %s from indices: %s' % (alias_name, str(index_list)))
+    connection.indices.delete_alias(index=index_list, name=alias_name, **kwargs)
+    logger.debug('Alias %s removed from indices %s' % (alias_name, str(index_list)))
