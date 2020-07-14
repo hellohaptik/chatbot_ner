9a48bf2af2822f609ef00d5d93e97d47b75ee8f3
Chirag Jain <jain.chirag925@gmail.com>
2020-07-14 20:36:47 +0530

    Add an benchmarking script - incomplete

--- ner_v1/experimental/bench.py
+++ ner_v1/experimental/bench.py
@@ -9,14 +9,14 @@
 # in the tpool, how many ner web workers, tpool <-> ner throughput, ner workers <-> ES throughput
 # Also todo is to think how can caching be applied on top of single query method
 
+import collections
 import json
 from concurrent.futures import ThreadPoolExecutor
+from timeit import default_timer as timer
 
 import requests
 
 """
-Texts can be loop/msearch
-
 multiple entities E, multiple texts T
     ## E1 E2 E3 E4 ... E
     T1  .  .  .  . ... .
@@ -32,6 +32,13 @@ multiple entities E, multiple texts T
     E - tpool loop over entities with T queries msearch (over texts)
     1 - E * T queries msearch flattened (text entity pairs)
     1 - T queries msearch (over texts)
+    
+    
+Output as:
+[
+    [entity_name, value, [variants, ...]],
+    ...
+]
 """
 
 scheme = 'http'
@@ -94,15 +101,27 @@ def base_query(text, entities):
 
 class Mode(object):
     @classmethod
-    def parse(cls, response):
-        print('=' * 80)
-        print(cls.__name__)
-        print('=' * 80)
-        print(response)
+    def parse(cls, responses, len_t, len_e):
+        # responses is [R, ... size T * E]
+        es_took = 0
+        r = []
+        for i in range(len_t):
+            d = collections.defaultdict(list)
+            for j in range(len_e):
+                es_r = responses[i * len_t + j][0]
+                es_took += es_r['took']
+                for hit in es_r['hits']['hits']:
+                    entity_name = hit['_source']['entity_data']
+                    entity_value = hit['_source']['value']
+                    entity_variants = hit['highlight']['variants']
+                    d[(entity_name, entity_value)].extend(entity_variants)
+            for (entity_name, entity_value) in sorted(d.keys()):
+                r.append([entity_name, entity_value, list(sorted(d[(entity_name, entity_value)]))])
+        return es_took, r
 
 
-class ET_1(Mode):
-    # E * T network calls 1 search query each
+class TE_One(Mode):
+    # T * E network calls 1 search query each
     @staticmethod
     def build(texts, entities):
         q = []
@@ -115,6 +134,34 @@ class ET_1(Mode):
                 body.append('\n')
                 q.append('\n'.join(body))
         return q
+
+    @classmethod
+    def parse(cls, responses, len_t, len_e):
+        # responses is [[R], ... size T x E]
+        # flatten
+        responses = [response[0] for response in responses]
+        return Mode.parse(responses, len_t, len_e)
+
+
+class One_TE(Mode):
+    # 1 network call with T * E search query each
+    @staticmethod
+    def build(texts, entities):
+        q = []
+        body = []
+        for text in texts:
+            for entity in entities:
+                body.append(json.dumps({'index': es_index, 'type': doc_type}))
+                body.append(json.dumps(base_query(text=text, entities=[entity])))
+
+        body.append('\n')
+        q.append('\n'.join(body))
+        return q
+
+    @classmethod
+    def parse(cls, responses, len_t, len_e):
+        # responses is [R, ... size T * E]
+        return Mode.parse(responses, len_t, len_e)
 
 
 class T_E(Mode):
@@ -131,6 +178,13 @@ class T_E(Mode):
             q.append('\n'.join(body))
         return q
 
+    @classmethod
+    def parse(cls, responses, len_t, len_e):
+        # responses is [[R, ... size E], ... size T]
+        # flatten
+        responses = [es_r for response in responses for es_r in response]
+        return Mode.parse(responses, len_t, len_e)
+
 
 class E_T(Mode):
     # E network calls T search query each
@@ -146,21 +200,12 @@ class E_T(Mode):
             q.append('\n'.join(body))
         return q
 
-
-class One_ET(Mode):
-    # 1 network call with E * T search query each
-    @staticmethod
-    def build(texts, entities):
-        q = []
-        body = []
-        for entity in entities:
-            for text in texts:
-                body.append(json.dumps({'index': es_index, 'type': doc_type}))
-                body.append(json.dumps(base_query(text=text, entities=[entity])))
-
-        body.append('\n')
-        q.append('\n'.join(body))
-        return q
+    @classmethod
+    def parse(cls, responses, len_t, len_e):
+        # responses is [[R, ... size T], ... size E]
+        # take a transpose and flatten
+        responses = [response[i] for i in range(len_t) for response in responses]
+        return Mode.parse(responses, len_t, len_e)
 
 
 class One_T(Mode):
@@ -177,9 +222,47 @@ class One_T(Mode):
         q.append('\n'.join(body))
         return q
 
+    @classmethod
+    def parse(cls, responses, len_t, len_e):
+        # responses is [R', ... size T]
+        es_took = 0
+        r = []
+        for es_r in responses:
+            d = collections.defaultdict(list)
+            es_took += es_r['took']
+            for hit in es_r['hits']['hits']:
+                entity_name = hit['_source']['entity_data']
+                entity_value = hit['_source']['value']
+                entity_variants = hit['highlight']['variants']
+                d[(entity_name, entity_value)].extend(entity_variants)
+            for (entity_name, entity_value) in sorted(d.keys()):
+                r.append([entity_name, entity_value, list(sorted(d[(entity_name, entity_value)]))])
+        return es_took, r
 
-# modes are ET/1, T/E, E/T, 1/ET, 1/T
-QUERY_MODES = [ET_1, T_E, E_T, One_ET, One_T]
+
+class T_One(Mode):
+    # T network call with 1 search query each
+    @staticmethod
+    def build(texts, entities):
+        q = []
+        for text in texts:
+            body = []
+            body.append(json.dumps({'index': es_index, 'type': doc_type}))
+            body.append(json.dumps(base_query(text=text, entities=entities)))
+            body.append('\n')
+            q.append('\n'.join(body))
+        return q
+
+    @classmethod
+    def parse(cls, responses, len_t, len_e):
+        # responses is [[R'], ... size T]
+        # flatten
+        responses = [response[0] for response in responses]
+        return One_T.parse(responses, len_t, len_e)
+
+
+# modes are TE/1, T/E, T/1, E/T, 1/TE, 1/T
+QUERY_MODES = [TE_One, One_TE, T_One, One_T, T_E, E_T, ]
 json_headers = {'Content-type': 'application/json'}
 
 
@@ -192,12 +275,13 @@ class Executor(object):
             self.pool = ThreadPoolExecutor(max_workers=tpool_size)
 
     def _execute(self, q):
-        print('=' * 80)
-        print(q)
-        print('=' * 80)
-        return requests.post(es_url, data=q, headers=json_headers).json()
+        # print('=' * 80)
+        # print(q)
+        # print('=' * 80)
+        return requests.post(es_url, data=q, headers=json_headers).json()['responses']
 
-    def execute(self, queries, parser_fn):
+    def execute(self, queries):
+        start = timer()
         results = []
         use_pool = len(queries) > 1 and self.pool
         if use_pool:
@@ -206,17 +290,32 @@ class Executor(object):
             for q in queries:
                 results.append(self._execute(q))
 
-        return [parser_fn(result) for result in results]
+        return timer() - start, results
 
-    def run_detection(self, texts, entities):
-        for qm in QUERY_MODES:
-            queries = qm.build(texts, entities)
-            self.execute(queries, parser_fn=qm.parse)
+    def run_detection(self, texts, entities, mode_cls):
+        queries = mode_cls.build(texts, entities)
+        exe_time, responses = self.execute(queries)
+        es_time, parsed = mode_cls.parse(responses, len(texts), len(entities))
+        return exe_time, es_time, parsed
+
+    def bench(self, texts, entities):
+        for mode_cls in QUERY_MODES:
+            exe_time, es_time, parsed = self.run_detection(texts, entities, mode_cls)
+            print("=" * 80)
+            print("-" * 80)
+            print(f'{mode_cls.__name__}, exec time: {exe_time}, es time: {es_time}')
+            print("-" * 80)
+            print(parsed)
+            print("-" * 80)
+            print("=" * 80)
+
+
+def make_expected_output(texts, entities):
+    e = Executor()
+    _, _, parsed = e.run_detection(texts, entities, TE_One)
+    return parsed
 
 # for pool_size in [0, 2, 4, 8, 16, 32, 64]:
-# make the queries
-# execute them
-# parse the response and convert it to a list of hits per text
 # instrument ES execution and total
 # run n times for average
 # record outputs for each for raw comparison
