5d65916f27a1532e243f0c7add584b9292964f09
Ubuntu <ubuntu@haptik-development-dev-adityasrivas-vm.3pdezlvpr5nulmpvjiqdicuwth.rx.internal.cloudapp.net>
2022-04-28 04:57:32 +0000

    changing term query to match query

--- datastore/elastic_search/query.py
+++ datastore/elastic_search/query.py
@@ -598,13 +598,18 @@ def get_crf_data_for_entity_name(connection, index_name, doc_type, entity_name, 
             }
         }
     }
-
-    if languages:
-        data['query']['bool']['filter'] = {
-            "terms": {
-                "language_script": languages
+    #       match query does not support array(list of languages).
+    #       building a list of dictionaries corresponding to match query for each language
+    list_query_langs = []
+    for lang in languages:
+        query_lang={
+            "match": {
+                "language_script": i
             }
         }
+        list_query_langs.append(query_lang)
+
+    data['query']['bool']['should'] =list_query_langs
 
     kwargs = dict(kwargs,
                   body=data,
